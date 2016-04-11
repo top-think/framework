ce6d3195e6cba4097230f00583ef5f276ec018a9
thinkphp <thinkphp@qq.com>
2016-04-11 11:11:00 +0800

    修正错误

--- library/think/Model.php
+++ library/think/Model.php
@@ -295,7 +295,7 @@ abstract class Model implements \JsonSerializable, \ArrayAccess
         // 避免影响其它操作方法
         $this->relation = [];
         // 保存关联对象值
-        $this->data[$name] = $result;
+        $this->data[$relation] = $result;
 
         return $result;
     }
