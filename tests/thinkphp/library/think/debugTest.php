83202fb9fc18c72a0c2b39dd9f1dad42e0754bf1
尘缘 <admin@4wei.cn>
2016-05-08 10:48:03 +0800

    修复一个 mac 上的 failure

--- tests/thinkphp/library/think/debugTest.php
+++ tests/thinkphp/library/think/debugTest.php
@@ -166,6 +166,8 @@ class debugTest extends \PHPUnit_Framework_TestCase
         $array      = explode("array", json_encode($output));
         if (IS_WIN) {
             $this->assertEquals("(1) {\\n  [\\\"key\\\"] => string(3) \\\"val\\\"\\n}\\n\\r\\n\"", end($array));
+        } else if (IS_MAC) {
+            $this->assertEquals("(1) {\\n  [\\\"key\\\"] => string(3) \\\"val\\\"\\n}\\n\\n\"", end($array));
         } else {
             $this->assertEquals("(1) {\\n  'key' =>\\n  string(3) \\\"val\\\"\\n}\\n\\n\"", end($array));
         }
