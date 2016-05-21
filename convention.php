10d62d854c0ce8969aa15cd57db438eef84079cd
thinkphp <thinkphp@qq.com>
2016-05-21 23:15:49 +0800

    去除error_page设置参数

--- convention.php
+++ convention.php
@@ -120,8 +120,6 @@ return [
 
     // 错误显示信息,非调试模式有效
     'error_message'          => '页面错误！请稍后再试～',
-    // 错误定向页面
-    'error_page'             => '',
     // 显示错误信息
     'show_error_msg'         => false,
 
