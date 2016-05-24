901631b8d5691ac32f01f98e5d4aa4cbd0af2b83
thinkphp <thinkphp@qq.com>
2016-05-24 17:55:05 +0800

    修正Route类

--- tests/thinkphp/baseTest.php
+++ tests/thinkphp/baseTest.php
@@ -46,12 +46,5 @@ class baseTest extends \PHPUnit_Framework_TestCase
         $this->assertTrue(!is_null(IS_CGI));
         $this->assertTrue(!is_null(IS_WIN));
         $this->assertTrue(!is_null(IS_CLI));
-        $this->assertTrue(is_bool(IS_AJAX));
-        $this->assertNotEmpty(NOW_TIME);
-        $this->assertNotEmpty(REQUEST_METHOD);
-        $this->assertTrue(is_bool(IS_GET));
-        $this->assertTrue(is_bool(IS_POST));
-        $this->assertTrue(is_bool(IS_PUT));
-        $this->assertTrue(is_bool(IS_DELETE));
     }
 }
