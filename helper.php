13103c84c7505208eb89253da7cb66a93428f071
thinkphp <thinkphp@qq.com>
2016-06-22 18:14:25 +0800

    注释修正

--- helper.php
+++ helper.php
@@ -29,8 +29,8 @@ use think\View;
 
 /**
  * 快速导入Traits PHP5.5以上无需调用
- * @param string $class trait库
- * @param string $ext 类库后缀
+ * @param string    $class trait库
+ * @param string    $ext 类库后缀
  * @return boolean
  */
 function load_trait($class, $ext = EXT)
@@ -41,9 +41,9 @@ function load_trait($class, $ext = EXT)
 /**
  * 抛出异常处理
  *
- * @param string  $msg  异常消息
- * @param integer $code 异常代码 默认为0
- * @param string $exception 异常类
+ * @param string    $msg  异常消息
+ * @param integer   $code 异常代码 默认为0
+ * @param string    $exception 异常类
  *
  * @throws Exception
  */
@@ -55,9 +55,9 @@ function exception($msg, $code = 0, $exception = '')
 
 /**
  * 记录时间（微秒）和内存使用情况
- * @param string $start 开始标签
- * @param string $end 结束标签
- * @param integer|string $dec 小数位 如果是m 表示统计内存占用
+ * @param string            $start 开始标签
+ * @param string            $end 结束标签
+ * @param integer|string    $dec 小数位 如果是m 表示统计内存占用
  * @return mixed
  */
 function debug($start, $end = '', $dec = 6)
@@ -71,9 +71,9 @@ function debug($start, $end = '', $dec = 6)
 
 /**
  * 获取语言变量值
- * @param string $name 语言变量名
- * @param array $vars 动态变量值
- * @param string $lang 语言
+ * @param string    $name 语言变量名
+ * @param array     $vars 动态变量值
+ * @param string    $lang 语言
  * @return mixed
  */
 function lang($name, $vars = [], $lang = '')
@@ -83,9 +83,9 @@ function lang($name, $vars = [], $lang = '')
 
 /**
  * 获取和设置配置参数
- * @param string|array $name 参数名
- * @param mixed $value 参数值
- * @param string $range 作用域
+ * @param string|array  $name 参数名
+ * @param mixed         $value 参数值
+ * @param string        $range 作用域
  * @return mixed
  */
 function config($name = '', $value = null, $range = '')
@@ -99,10 +99,9 @@ function config($name = '', $value = null, $range = '')
 
 /**
  * 获取输入数据 支持默认值和过滤
- * @param string $key 获取的变量名
- * @param mixed $default 默认值
- * @param string $filter 过滤方法
- * @param bool $merge 是否合并系统默认过滤方法
+ * @param string    $key 获取的变量名
+ * @param mixed     $default 默认值
+ * @param string    $filter 过滤方法
  * @return mixed
  */
 function input($key, $default = null, $filter = null)
@@ -123,17 +122,17 @@ function input($key, $default = null, $filter = null)
         // 默认为自动判断
         $method = 'param';
     }
-    if(isset($has)){
+    if (isset($has)) {
         return request()->has($key, $method, $default);
-    }else{
+    } else {
         return request()->$method($key, $default, $filter);
     }
 }
 
 /**
  * 渲染输出Widget
- * @param string $name Widget名称
- * @param array $data 传人的参数
+ * @param string    $name Widget名称
+ * @param array     $data 传人的参数
  * @return mixed
  */
 function widget($name, $data = [])
@@ -143,9 +142,9 @@ function widget($name, $data = [])
 
 /**
  * 实例化Model
- * @param string $name Model名称
- * @param string $layer 业务层名称
- * @param bool $appendSuffix 是否添加类名后缀
+ * @param string    $name Model名称
+ * @param string    $layer 业务层名称
+ * @param bool      $appendSuffix 是否添加类名后缀
  * @return \think\Model
  */
 function model($name = '', $layer = 'model', $appendSuffix = false)
@@ -155,9 +154,9 @@ function model($name = '', $layer = 'model', $appendSuffix = false)
 
 /**
  * 实例化验证器
- * @param string $name 验证器名称
- * @param string $layer 业务层名称
- * @param bool $appendSuffix 是否添加类名后缀
+ * @param string    $name 验证器名称
+ * @param string    $layer 业务层名称
+ * @param bool      $appendSuffix 是否添加类名后缀
  * @return \think\Validate
  */
 function validate($name = '', $layer = 'validate', $appendSuffix = false)
@@ -167,8 +166,8 @@ function validate($name = '', $layer = 'validate', $appendSuffix = false)
 
 /**
  * 实例化数据库类
- * @param string $name 操作的数据表名称（不含前缀）
- * @param array|string $config 数据库配置参数
+ * @param string        $name 操作的数据表名称（不含前缀）
+ * @param array|string  $config 数据库配置参数
  * @return \think\db\Query
  */
 function db($name = '', $config = [])
@@ -178,9 +177,9 @@ function db($name = '', $config = [])
 
 /**
  * 实例化控制器 格式：[模块/]控制器
- * @param string $name 资源地址
- * @param string $layer 控制层名称
- * @param bool $appendSuffix 是否添加类名后缀
+ * @param string    $name 资源地址
+ * @param string    $layer 控制层名称
+ * @param bool      $appendSuffix 是否添加类名后缀
  * @return \think\Controller
  */
 function controller($name, $layer = 'controller', $appendSuffix = false)
@@ -190,10 +189,10 @@ function controller($name, $layer = 'controller', $appendSuffix = false)
 
 /**
  * 调用模块的操作方法 参数格式 [模块/控制器/]操作
- * @param string $url 调用地址
- * @param string|array $vars 调用参数 支持字符串和数组
- * @param string $layer 要调用的控制层名称
- * @param bool $appendSuffix 是否添加类名后缀 
+ * @param string        $url 调用地址
+ * @param string|array  $vars 调用参数 支持字符串和数组
+ * @param string        $layer 要调用的控制层名称
+ * @param bool          $appendSuffix 是否添加类名后缀
  * @return mixed
  */
 function action($url, $vars = [], $layer = 'controller', $appendSuffix = false)
@@ -203,9 +202,9 @@ function action($url, $vars = [], $layer = 'controller', $appendSuffix = false)
 
 /**
  * 导入所需的类库 同java的Import 本函数有缓存功能
- * @param string $class 类库命名空间字符串
- * @param string $baseUrl 起始路径
- * @param string $ext 导入的文件扩展名
+ * @param string    $class 类库命名空间字符串
+ * @param string    $baseUrl 起始路径
+ * @param string    $ext 导入的文件扩展名
  * @return boolean
  */
 function import($class, $baseUrl = '', $ext = EXT)
@@ -215,8 +214,8 @@ function import($class, $baseUrl = '', $ext = EXT)
 
 /**
  * 快速导入第三方框架类库 所有第三方框架的类库文件统一放到 系统的Vendor目录下面
- * @param string $class 类库
- * @param string $ext 类库后缀
+ * @param string    $class 类库
+ * @param string    $ext 类库后缀
  * @return boolean
  */
 function vendor($class, $ext = EXT)
@@ -226,9 +225,9 @@ function vendor($class, $ext = EXT)
 
 /**
  * 浏览器友好的变量输出
- * @param mixed $var 变量
- * @param boolean $echo 是否输出 默认为true 如果为false 则返回输出字符串
- * @param string $label 标签 默认为空
+ * @param mixed     $var 变量
+ * @param boolean   $echo 是否输出 默认为true 如果为false 则返回输出字符串
+ * @param string    $label 标签 默认为空
  * @return void|string
  */
 function dump($var, $echo = true, $label = null)
@@ -238,10 +237,10 @@ function dump($var, $echo = true, $label = null)
 
 /**
  * Url生成
- * @param string $url 路由地址
- * @param string|array $value 变量
- * @param bool|string $suffix 前缀
- * @param bool|string $domain 域名
+ * @param string        $url 路由地址
+ * @param string|array  $value 变量
+ * @param bool|string   $suffix 前缀
+ * @param bool|string   $domain 域名
  * @return string
  */
 function url($url = '', $vars = '', $suffix = true, $domain = false)
@@ -251,9 +250,9 @@ function url($url = '', $vars = '', $suffix = true, $domain = false)
 
 /**
  * Session管理
- * @param string|array $name session名称，如果为数组表示进行session设置
- * @param mixed $value session值
- * @param string $prefix 前缀
+ * @param string|array  $name session名称，如果为数组表示进行session设置
+ * @param mixed         $value session值
+ * @param string        $prefix 前缀
  * @return mixed
  */
 function session($name, $value = '', $prefix = null)
@@ -278,9 +277,9 @@ function session($name, $value = '', $prefix = null)
 
 /**
  * Cookie管理
- * @param string|array $name cookie名称，如果为数组表示进行cookie设置
- * @param mixed $value cookie值
- * @param mixed $option 参数
+ * @param string|array  $name cookie名称，如果为数组表示进行cookie设置
+ * @param mixed         $value cookie值
+ * @param mixed         $option 参数
  * @return mixed
  */
 function cookie($name, $value = '', $option = null)
@@ -305,9 +304,9 @@ function cookie($name, $value = '', $option = null)
 
 /**
  * 缓存管理
- * @param mixed $name 缓存名称，如果为数组表示进行缓存设置
- * @param mixed $value 缓存值
- * @param mixed $options 缓存参数
+ * @param mixed     $name 缓存名称，如果为数组表示进行缓存设置
+ * @param mixed     $value 缓存值
+ * @param mixed     $options 缓存参数
  * @return mixed
  */
 function cache($name, $value = '', $options = null)
@@ -338,8 +337,8 @@ function cache($name, $value = '', $options = null)
 
 /**
  * 记录日志信息
- * @param mixed $log log信息 支持字符串和数组
- * @param string $level 日志级别
+ * @param mixed     $log log信息 支持字符串和数组
+ * @param string    $level 日志级别
  * @return void|array
  */
 function trace($log = '[think]', $level = 'log')
@@ -375,9 +374,9 @@ function response($data = [], $code = 200, $header = [], $type = 'html')
 
 /**
  * 渲染模板输出
- * @param string $template 模板文件
- * @param array $vars 模板变量
- * @param integer $code 状态码
+ * @param string    $template 模板文件
+ * @param array     $vars 模板变量
+ * @param integer   $code 状态码
  * @return \think\response\View
  */
 function view($template = '', $vars = [], $code = 200)
@@ -426,9 +425,9 @@ function xml($data = [], $code = 200, $header = [], $options = [])
 
 /**
  * 获取\think\response\Redirect对象实例
- * @param mixed $url 重定向地址 支持Url::build方法的地址
+ * @param mixed         $url 重定向地址 支持Url::build方法的地址
  * @param array|integer $params 额外参数
- * @param integer $code 状态码
+ * @param integer       $code 状态码
  * @return \think\response\Redirect
  */
 function redirect($url = [], $params = [], $code = 302)
@@ -442,9 +441,9 @@ function redirect($url = [], $params = [], $code = 302)
 
 /**
  * 抛出HTTP异常
- * @param integer $code 状态码
- * @param string $message 错误信息
- * @param array $header 参数
+ * @param integer   $code 状态码
+ * @param string    $message 错误信息
+ * @param array     $header 参数
  */
 function abort($code, $message = null, $header = [])
 {
