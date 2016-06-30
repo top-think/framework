bd85152d3016f423b1fb853ffba99da491b5f234
xiaobo.sun <5ini99@sohu.com>
2016-06-30 15:20:46 +0800

    Update Jsonp.php
    当url_common_param为false时是无法获取到$_GET的数据的，须使用Request来获取

--- library/think/response/Jsonp.php
+++ library/think/response/Jsonp.php
@@ -34,7 +34,7 @@ class Jsonp extends Response
     protected function output($data)
     {
         // 返回JSON数据格式到客户端 包含状态信息 [当url_common_param为false时是无法获取到$_GET的数据的，故使用Request来获取<xiaobo.sun@qq.com>]
-        $var_jsonp_handler = Request::instance()->param("{$this->options['var_jsonp_handler']}", "");
+        $var_jsonp_handler = Request::instance()->param($this->options['var_jsonp_handler'], "");
         $handler = !empty($var_jsonp_handler) ? $var_jsonp_handler : $this->options['default_jsonp_handler'];
         $data    = $handler . '(' . json_encode($data, $this->options['json_encode_param']) . ');';
         return $data;
