9bdc1e0c64d0c0403473d9062e39bf1d727eccad
thinkphp <thinkphp@qq.com>
2016-07-14 16:56:15 +0800

    修正Route类一处错误

--- library/think/Route.php
+++ library/think/Route.php
@@ -672,15 +672,14 @@ class Route
         if ($checkDomain) {
             self::checkDomain($request);
         }
+        // 获取当前请求类型的路由规则
+        $rules = self::$rules[$request->method()];
 
         // 检测URL绑定
         $return = self::checkUrlBind($url, $rules, $depr);
         if (false !== $return) {
             return $return;
         }
-
-        // 获取当前请求类型的路由规则
-        $rules = self::$rules[$request->method()];
 
         if (isset($rules[$url])) {
             // 静态路由规则检测
