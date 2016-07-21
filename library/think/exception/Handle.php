1b20d1ef242f100936c69cded87ca37967db96e8
yunwuxin <448901948@qq.com>
2016-07-21 10:55:56 +0800

    自定义异常页面保持原来的状态码

--- library/think/exception/Handle.php
+++ library/think/exception/Handle.php
@@ -103,7 +103,7 @@ class Handle
         $status   = $e->getStatusCode();
         $template = Config::get('http_exception_template');
         if (!App::$debug && !empty($template[$status])) {
-            return Response::create($template[$status], 'view')->assign(['e' => $e]);
+            return Response::create($template[$status], 'view', $status)->assign(['e' => $e]);
         } else {
             return $this->convertExceptionToResponse($e);
         }
