4cf9d17aecb9f6f762b1e5fc38476d0c3a61fc5a
thinkphp <thinkphp@qq.com>
2016-07-29 15:07:00 +0800

    改进Request类的create方法

--- library/think/Request.php
+++ library/think/Request.php
@@ -236,7 +236,7 @@ class Request
         $options['server']      = $server;
         $options['url']         = $server['REQUEST_URI'];
         $options['baseUrl']     = $info['path'];
-        $options['pathinfo']    = ltrim($info['path'], '/');
+        $options['pathinfo']    = '/' == $info['path'] ? '/' : ltrim($info['path'], '/');
         $options['method']      = $server['REQUEST_METHOD'];
         $options['domain']      = $server['HTTP_HOST'];
         $options['content']     = $content;
