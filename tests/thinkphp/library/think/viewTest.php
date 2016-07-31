219084eb4b330ee14caf395d7f4aa2b401b0ed0f
thinkphp <thinkphp@qq.com>
2016-07-31 20:07:03 +0800

    改进View单元测试

--- tests/thinkphp/library/think/viewTest.php
+++ tests/thinkphp/library/think/viewTest.php
@@ -38,9 +38,12 @@ class viewTest extends \PHPUnit_Framework_TestCase
      */
     public function testAssign()
     {
-        $view_instance = \think\View::instance();
-        $data          = $view_instance->assign(array('key' => 'value'));
-        $data          = $view_instance->assign('key2', 'value2');
+        $view_instance      = \think\View::instance();
+        $view_instance->key = 'value';
+        $this->assertTrue(isset($view_instance->key));
+        $this->assertEquals('value', $view_instance->key);
+        $data = $view_instance->assign(array('key' => 'value'));
+        $data = $view_instance->assign('key2', 'value2');
         //测试私有属性
         $expect_data = array('key' => 'value', 'key2' => 'value2');
         $this->assertAttributeEquals($expect_data, 'data', $view_instance);
@@ -54,6 +57,7 @@ class viewTest extends \PHPUnit_Framework_TestCase
     public function testEngine()
     {
         $view_instance = \think\View::instance();
+        $data          = $view_instance->engine('php');
         $data          = $view_instance->engine(['type' => 'php', 'view_path' => '', 'view_suffix' => '.php', 'view_depr' => DS]);
         $php_engine    = new \think\view\driver\Php(['view_path' => '', 'view_suffix' => '.php', 'view_depr' => DS]);
         $this->assertAttributeEquals($php_engine, 'engine', $view_instance);
@@ -61,6 +65,12 @@ class viewTest extends \PHPUnit_Framework_TestCase
         $data         = $view_instance->engine(['type' => 'think', 'view_path' => '', 'view_suffix' => '.html', 'view_depr' => DS]);
         $think_engine = new \think\view\driver\Think(['view_path' => '', 'view_suffix' => '.html', 'view_depr' => DS]);
         $this->assertAttributeEquals($think_engine, 'engine', $view_instance);
+    }
+
+    public function testReplace()
+    {
+        $view_instance = \think\View::instance();
+        $view_instance->replace('string', 'replace')->display('string');
     }
 
 }
