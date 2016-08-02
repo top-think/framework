53f6d41ef429ffa4c61bcf2d527971c04164fcfb
thinkphp <thinkphp@qq.com>
2016-08-02 18:04:40 +0800

    改进Validate类的boolean验证规则 支持表单数据

--- library/think/Validate.php
+++ library/think/Validate.php
@@ -551,7 +551,7 @@ class Validate
                 break;
             case 'boolean':
                 // 是否为布尔值
-                $result = $this->filter($value, FILTER_VALIDATE_BOOLEAN);
+                $result = in_array($value, ['true', 'false', 0, 1, true, false]);
                 break;
             case 'array':
                 // 是否为数组
