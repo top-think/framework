4da468b27dc4b51c7f6125f8f7079e2eb7d2fc42
thinkphp <thinkphp@qq.com>
2016-08-09 17:08:46 +0800

    改进db助手函数增加第三个参数 用于设置是否强制重新连接

--- helper.php
+++ helper.php
@@ -187,11 +187,12 @@ if (!function_exists('db')) {
      * 实例化数据库类
      * @param string        $name 操作的数据表名称（不含前缀）
      * @param array|string  $config 数据库配置参数
+     * @param bool          $force 是否强制重新连接
      * @return \think\db\Query
      */
-    function db($name = '', $config = [])
+    function db($name = '', $config = [], $force = true)
     {
-        return Db::connect($config, true)->name($name);
+        return Db::connect($config, $force)->name($name);
     }
 }
 
