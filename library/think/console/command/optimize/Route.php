784294f846f9fb7b0abc27b0ffaa37b2566d57aa
thinkphp <thinkphp@qq.com>
2016-08-10 14:21:13 +0800

    修正Route指令

--- library/think/console/command/optimize/Route.php
+++ library/think/console/command/optimize/Route.php
@@ -37,7 +37,7 @@ class Route extends Command
         foreach ($files as $file) {
             if (is_file(CONF_PATH . $file . CONF_EXT)) {
                 $config = include CONF_PATH . $file . CONF_EXT;
-                if ($config) {
+                if (is_array($config)) {
                     \think\Route::import($config);
                 }
             }
