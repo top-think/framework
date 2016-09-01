07a30734a35cf542bfb93d6127e0e72fbbcfef98
dingdayu <614422099@qq.com>
2016-09-01 17:43:53 +0800

    fixed #253 * 增加cache/session redis驱动的库选择逻辑; * hint: 需要在缓存配置(cache节点)中增加'select'; * eg: ['cache' => ['select'=>0, ...]];

--- library/think/session/driver/Redis.php
+++ library/think/session/driver/Redis.php
@@ -22,6 +22,7 @@ class Redis extends SessionHandler
         'host'         => '127.0.0.1', // redis主机
         'port'         => 6379, // redis端口
         'password'     => '', // 密码
+        'select'       => 0, // 操作库
         'expire'       => 3600, // 有效期(秒)
         'timeout'      => 0, // 超时时间(秒)
         'persistent'   => true, // 是否长连接
@@ -56,6 +57,11 @@ class Redis extends SessionHandler
         if ('' != $this->config['password']) {
             $this->handler->auth($this->config['password']);
         }
+
+        if (0 != $this->config['select']) {
+            $this->handler->select($this->config['select']);
+        }
+
         return true;
     }
 
