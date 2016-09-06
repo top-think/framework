3bfa4a19f04b04ea9f3a881376260f98572ffd93
yunwuxin <448901948@qq.com>
2016-09-06 17:15:43 +0800

    前置操作不存在时抛出异常

--- library/think/Controller.php
+++ library/think/Controller.php
@@ -38,7 +38,7 @@ class Controller
 
     /**
      * 架构函数
-     * @param Request    $request     Request对象
+     * @param Request $request Request对象
      * @access public
      */
     public function __construct(Request $request = null)
@@ -70,8 +70,8 @@ class Controller
     /**
      * 前置操作
      * @access protected
-     * @param string    $method     前置操作方法名
-     * @param array     $options    调用参数 ['only'=>[...]] 或者['except'=>[...]]
+     * @param string $method  前置操作方法名
+     * @param array  $options 调用参数 ['only'=>[...]] 或者['except'=>[...]]
      */
     protected function beforeAction($method, $options = [])
     {
@@ -91,18 +91,16 @@ class Controller
             }
         }
 
-        if (method_exists($this, $method)) {
-            call_user_func([$this, $method]);
-        }
+        call_user_func([$this, $method]);
     }
 
     /**
      * 加载模板输出
      * @access protected
-     * @param string    $template 模板文件名
-     * @param array     $vars     模板输出变量
-     * @param array     $replace     模板替换
-     * @param array     $config     模板参数
+     * @param string $template 模板文件名
+     * @param array  $vars     模板输出变量
+     * @param array  $replace  模板替换
+     * @param array  $config   模板参数
      * @return mixed
      */
     protected function fetch($template = '', $vars = [], $replace = [], $config = [])
@@ -113,10 +111,10 @@ class Controller
     /**
      * 渲染内容输出
      * @access protected
-     * @param string    $content 模板内容
-     * @param array     $vars     模板输出变量
-     * @param array     $replace 替换内容
-     * @param array     $config     模板参数
+     * @param string $content 模板内容
+     * @param array  $vars    模板输出变量
+     * @param array  $replace 替换内容
+     * @param array  $config  模板参数
      * @return mixed
      */
     protected function display($content = '', $vars = [], $replace = [], $config = [])
@@ -127,8 +125,8 @@ class Controller
     /**
      * 模板变量赋值
      * @access protected
-     * @param mixed     $name  要显示的模板变量
-     * @param mixed     $value 变量的值
+     * @param mixed $name  要显示的模板变量
+     * @param mixed $value 变量的值
      * @return void
      */
     protected function assign($name, $value = '')
