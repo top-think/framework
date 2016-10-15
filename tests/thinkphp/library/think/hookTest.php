5fc92d60e7a5d5852ef1e4bc9bcb2a81449a317e
Dongasai <1514582970@qq.com>
2016-10-15 20:13:48 +0800

    Hook类增加静态支持,并修改了单元测试文件,

--- tests/thinkphp/library/think/hookTest.php
+++ tests/thinkphp/library/think/hookTest.php
@@ -1,4 +1,5 @@
 <?php
+
 // +----------------------------------------------------------------------
 // | ThinkPHP [ WE CAN DO IT JUST THINK ]
 // +----------------------------------------------------------------------
@@ -16,7 +17,9 @@
 
 namespace tests\thinkphp\library\think;
 
-use think\Hook;
+use \think\Hook;
+
+require_once '../../../mock.php';
 
 class hookTest extends \PHPUnit_Framework_TestCase
 {
@@ -34,15 +37,33 @@ class hookTest extends \PHPUnit_Framework_TestCase
         $this->assertEquals([
             '\tests\thinkphp\library\think\behavior\Three',
             '\tests\thinkphp\library\think\behavior\One',
-            '\tests\thinkphp\library\think\behavior\Two'],
-            Hook::get('my_pos'));
+            '\tests\thinkphp\library\think\behavior\Two'], Hook::get('my_pos'));
+    }
+
+    public function testStatic()
+    {
+
+        Hook::add('my_pos2', '\tests\thinkphp\library\think\behavior\StaticDemo');
+        $data2 = [];
+        Hook::setStatic('my_pos2');
+        $this->assertEquals(true, Hook::isStatic('my_pos2'));
+        Hook::listen('my_pos2', $data2);
+        $this->assertEquals('run', $data2['function']);
+
+        //第二组测试
+        Hook::add('my_pos3', '\tests\thinkphp\library\think\behavior\StaticDemo');
+        $data3 = [];
+        Hook::setStatic('my_pos3');
+        $this->assertEquals(true, Hook::isStatic('my_pos3'));
+        Hook::listen('my_pos3', $data3);
+        $this->assertEquals('my_pos3', $data3['function']);
     }
 
     public function testImport()
     {
         Hook::import(['my_pos' => [
-            '\tests\thinkphp\library\think\behavior\One',
-            '\tests\thinkphp\library\think\behavior\Three'],
+                '\tests\thinkphp\library\think\behavior\One',
+                '\tests\thinkphp\library\think\behavior\Three'],
         ]);
         Hook::import(['my_pos' => ['\tests\thinkphp\library\think\behavior\Two']], false);
         Hook::import(['my_pos' => ['\tests\thinkphp\library\think\behavior\Three', '_overlay' => true]]);
@@ -50,7 +71,6 @@ class hookTest extends \PHPUnit_Framework_TestCase
         $data['name'] = 'thinkphp';
         Hook::listen('my_pos', $data);
         $this->assertEquals(3, $data['id']);
-
     }
 
     public function testExec()
@@ -60,8 +80,10 @@ class hookTest extends \PHPUnit_Framework_TestCase
         $this->assertEquals(true, Hook::exec('\tests\thinkphp\library\think\behavior\One'));
         $this->assertEquals(false, Hook::exec('\tests\thinkphp\library\think\behavior\One', 'test', $data));
         $this->assertEquals('test', $data['name']);
-        $this->assertEquals('Closure', Hook::exec(function (&$data) {$data['name'] = 'Closure';return 'Closure';}));
-
+        $this->assertEquals('Closure', Hook::exec(function (&$data) {
+                    $data['name'] = 'Closure';
+                    return 'Closure';
+                }));
     }
 
 }
