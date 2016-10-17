bb9e33e9a5fe07471daf4669832e9e430d49a89e
yunwuxin <448901948@qq.com>
2016-10-17 17:52:09 +0800

    修正json_encode时 "Failed calling XXX::jsonSerialize()" 的异常

--- library/think/response/Json.php
+++ library/think/response/Json.php
@@ -27,17 +27,25 @@ class Json extends Response
      * @access protected
      * @param mixed $data 要处理的数据
      * @return mixed
+     * @throws \Exception
      */
     protected function output($data)
     {
-        // 返回JSON数据格式到客户端 包含状态信息
-        $data = json_encode($data, $this->options['json_encode_param']);
+        try {
+            // 返回JSON数据格式到客户端 包含状态信息
+            $data = json_encode($data, $this->options['json_encode_param']);
 
-        if ($data === false) {
-            throw new \InvalidArgumentException(json_last_error_msg());
+            if ($data === false) {
+                throw new \InvalidArgumentException(json_last_error_msg());
+            }
+
+            return $data;
+        } catch (\Exception $e) {
+            if ($e->getPrevious()) {
+                throw $e->getPrevious();
+            }
+            throw $e;
         }
-
-        return $data;
     }
 
 }
