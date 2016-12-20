c695397733a707d4434fa8f8c070c9caf1f1f369
thinkphp <thinkphp@qq.com>
2016-12-20 08:28:50 +0800

    版本更新

--- base.php
+++ base.php
@@ -9,7 +9,7 @@
 // | Author: liu21st <liu21st@gmail.com>
 // +----------------------------------------------------------------------
 
-define('THINK_VERSION', '5.0.4beta');
+define('THINK_VERSION', '5.0.4');
 define('THINK_START_TIME', microtime(true));
 define('THINK_START_MEM', memory_get_usage());
 define('EXT', '.php');
