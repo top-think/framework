428a30e2965ab5cba69954f4bca82f75e042b087
thinkphp <thinkphp@qq.com>
2017-02-24 14:02:26 +0800

    改进Request类param方法

--- library/think/Request.php
+++ library/think/Request.php
@@ -633,7 +633,7 @@ class Request
         if (true === $name) {
             // 获取包含文件上传信息的数组
             $file = $this->file();
-            $data = array_merge($this->param, $file);
+            $data = is_array($file) ? array_merge($this->param, $file) : $this->param;
             return $this->input($data, '', $default, $filter);
         }
         return $this->input($this->param, $name, $default, $filter);
@@ -886,7 +886,7 @@ class Request
                 return $array[$name];
             }
         }
-        return;
+        return null;
     }
 
     /**
