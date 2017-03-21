2971e93fd5d91fef8c97873787466d75d0647a85
水平凡 <admin@abc3210.com>
2017-03-21 14:08:32 +0800

    修复inc方法

--- library/think/cache/driver/Memcached.php
+++ library/think/cache/driver/Memcached.php
@@ -125,7 +125,10 @@ class Memcached extends Driver
     public function inc($name, $step = 1)
     {
         $key = $this->getCacheKey($name);
-        return $this->handler->increment($key, $step);
+        if( $this->has($name) ){
+            return $this->handler->increment($key, $step);
+        }
+        return $this->handler->set($key, $step);
     }
 
     /**
