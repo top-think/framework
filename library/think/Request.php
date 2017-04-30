68790e8d9a6b4075b16f15e1bd419492e95fe0ec
thinkphp <thinkphp@qq.com>
2017-04-30 22:58:49 +0800

    改进输入过滤

--- library/think/Request.php
+++ library/think/Request.php
@@ -1034,11 +1034,7 @@ class Request
             $filter = [];
         } else {
             $filter = $filter ?: $this->filter;
-            if (is_string($filter)) {
-                $filter = explode(',', $filter);
-            } else {
-                $filter = (array) $filter;
-            }
+            $filter = (array) $filter;
         }
 
         $filter[] = $default;
