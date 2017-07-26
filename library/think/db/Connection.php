0e02ea65dad608210ce3c3232fc5c496d8b1bd8b
hmc <hmczju@gmail.com>
2017-07-26 16:21:31 +0800

    Update Connection.php

--- library/think/db/Connection.php
+++ library/think/db/Connection.php
@@ -637,6 +637,16 @@ abstract class Connection
                 );
             }
 
+        } catch (\PDOException $e) {
+            if ($this->isBreak($e)) {
+                return $this->close()->startTrans();
+            }
+            throw $e;
+        } catch (\ErrorException $e) {
+            if ($this->isBreak($e)) {
+                return $this->close()->startTrans();
+            }
+            throw $e;
         } catch (\Exception $e){
             if ($this->isBreak($e)) {
                 return $this->close()->startTrans();
