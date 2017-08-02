2b2f9e0de71c5d7f985da2ddf768a51218ae5948
yinggaozhen <yinggaozhen@hotmail.com>
2017-08-02 19:27:11 +0800

    修复Loader命名空间重复

--- library/think/Controller.php
+++ library/think/Controller.php
@@ -11,13 +11,14 @@
 
 namespace think;
 
-\think\Loader::import('controller/Jump', TRAIT_PATH, EXT);
-
 use think\exception\ValidateException;
+use traits\controller\Jump;
+
+Loader::import('controller/Jump', TRAIT_PATH, EXT);
 
 class Controller
 {
-    use \traits\controller\Jump;
+    use Jump;
 
     /**
      * @var \think\View 视图类实例
