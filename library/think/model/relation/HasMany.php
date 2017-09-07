44856d9d072605871e5aa842e59d36d96cf2eebc
thinkphp <thinkphp@qq.com>
2017-09-07 11:24:31 +0800

    关联调整

--- library/think/model/relation/HasMany.php
+++ library/think/model/relation/HasMany.php
@@ -260,7 +260,6 @@ class HasMany extends Relation
 
         return $this->parent->db()
             ->alias($model)
-            ->field($model . '.*')
             ->join($table . ' ' . $relation, $model . '.' . $this->localKey . '=' . $relation . '.' . $this->foreignKey, $joinType)
             ->group($relation . '.' . $this->foreignKey)
             ->having('count(' . $id . ')' . $operator . $count);
