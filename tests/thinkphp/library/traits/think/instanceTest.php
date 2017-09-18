b673555aab7fd16c70f960e411e3177478a83b74
Gaozhen Ying <yinggaozhen@hotmail.com>
2017-09-18 21:19:39 +0800

    Add Cache test case

--- tests/thinkphp/library/traits/think/instanceTest.php
+++ tests/thinkphp/library/traits/think/instanceTest.php
@@ -11,15 +11,8 @@ class instanceTest extends \PHPUnit_Framework_TestCase
         $this->assertInstanceOf('\tests\thinkphp\library\traits\think\InstanceTestFather', $father);
         $this->assertEquals([], $father->options);
 
-        $father2 = InstanceTestFather::instance(['father']);
-        $this->assertEquals([], $father2->options);
-
-        $father2->options = ['father'];
-        $this->assertEquals(['father'], $father->options);
-
-        $son = InstanceTestSon::instance(['son']);
-        $this->assertInstanceOf('\tests\thinkphp\library\traits\think\InstanceTestFather', $son);
-        $this->assertEquals(['father'], $son->options);
+        $son = InstanceTestFather::instance(['son']);
+        $this->assertSame($father, $son);
     }
 
     public function testCallStatic()
