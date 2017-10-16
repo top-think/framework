412b37b9a9a5f6306322aac3bcb3ef6079f412e6
thinkphp <thinkphp@qq.com>
2017-10-16 15:53:42 +0800

    RC2

--- library/think/App.php
+++ library/think/App.php
@@ -20,7 +20,7 @@ use think\exception\HttpResponseException;
  */
 class App implements \ArrayAccess
 {
-    const VERSION = '5.1.0RC1';
+    const VERSION = '5.1.0RC2';
 
     /**
      * @var string 当前模块路径
