04bb1353e1ecb55721a41041d30d231ea984f811
thinkphp <thinkphp@qq.com>
2017-10-31 12:11:51 +0800

    改进参数绑定类型对枚举类型的支持

--- library/think/db/Connection.php
+++ library/think/db/Connection.php
@@ -285,7 +285,9 @@ abstract class Connection
      */
     public function getFieldBindType($type)
     {
-        if (preg_match('/(int|double|float|decimal|real|numeric|serial|bit)/is', $type)) {
+        if (0 === strpos($type, 'set') || 0 === strpos($type, 'enum')) {
+            $bind = PDO::PARAM_STR;
+        } elseif (preg_match('/(int|double|float|decimal|real|numeric|serial|bit)/is', $type)) {
             $bind = PDO::PARAM_INT;
         } elseif (preg_match('/bool/is', $type)) {
             $bind = PDO::PARAM_BOOL;
