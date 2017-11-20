c937fbdbd50984950991f182d3f09b8955bd4000
Lin07ux <linshengli.linux7@qq.com>
2017-11-20 15:11:56 +0800

    补全 Db、Debug、Env、Error、Exception、File、Hook、Lang、Loader、Log 类的文档，调整代码样式

--- library/think/Env.php
+++ library/think/Env.php
@@ -15,22 +15,25 @@ class Env
 {
     /**
      * 获取环境变量值
-     * @param string    $name 环境变量名（支持二级 .号分割）
-     * @param string    $default  默认值
+     * @access public
+     * @param  string $name    环境变量名（支持二级 . 号分割）
+     * @param  string $default 默认值
      * @return mixed
      */
     public static function get($name, $default = null)
     {
         $result = getenv(ENV_PREFIX . strtoupper(str_replace('.', '_', $name)));
+
         if (false !== $result) {
             if ('false' === $result) {
                 $result = false;
             } elseif ('true' === $result) {
                 $result = true;
             }
+
             return $result;
-        } else {
-            return $default;
         }
+
+        return $default;
     }
 }
