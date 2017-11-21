8cd58e2206f95b60aea51512bfa86885647f2c4b
thinkphp <thinkphp@qq.com>
2017-11-21 15:36:36 +0800

    改进

--- library/think/model/Pivot.php
+++ library/think/model/Pivot.php
@@ -24,11 +24,11 @@ class Pivot extends Model
     /**
      * 架构函数
      * @access public
-     * @param Model         $parent 上级模型
      * @param array|object  $data 数据
+     * @param Model         $parent 上级模型
      * @param string        $table 中间数据表名
      */
-    public function __construct(Model $parent = null, $data = [], $table = '')
+    public function __construct($data = [], Model $parent = null, $table = '')
     {
         $this->parent = $parent;
 
