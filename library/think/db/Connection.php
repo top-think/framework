a3f30ee7985fb5992c07478a70a49464f5f98b1f
carson <docxcn@gmail.com>
2017-11-24 14:59:13 +0800

    增加strict配置

--- library/think/db/Connection.php
+++ library/think/db/Connection.php
@@ -478,6 +478,8 @@ abstract class Connection
 
                 $this->links[$linkNum] = new PDO($config['dsn'], $config['username'], $config['password'], $params);
 
+                $this->setModes($this->links[$linkNum], $config);
+
                 if ($config['debug']) {
                     // 记录数据库连接信息
                     $this->log('[ DB ] CONNECT:[ UseTime:' . number_format(microtime(true) - $startTime, 6) . 's ] ' . $config['dsn']);
@@ -2049,6 +2051,49 @@ abstract class Connection
         }
 
         return $dsn;
+    }
+
+    /**
+     * Set the modes for the connection.
+     *
+     * @param  PDO  $connection
+     * @param  array  $config
+     * @return void
+     */
+    protected function setModes(PDO $connection, array $config)
+    {
+        if (isset($config['modes'])) {
+            $this->setCustomModes($connection, $config);
+        } elseif (isset($config['strict'])) {
+            if ($config['strict']) {
+                $connection->prepare($this->strictMode())->execute();
+            } else {
+                $connection->prepare("set session sql_mode='NO_ENGINE_SUBSTITUTION'")->execute();
+            }
+        }
+    }
+
+    /**
+     * Set the custom modes on the connection.
+     *
+     * @param  PDO  $connection
+     * @param  array  $config
+     * @return void
+     */
+    protected function setCustomModes(PDO $connection, array $config)
+    {
+        $modes = implode(',', $config['modes']);
+        $connection->prepare("set session sql_mode='{$modes}'")->execute();
+    }
+
+    /**
+     * Get the query to enable strict mode.
+     *
+     * @return string
+     */
+    protected function strictMode()
+    {
+        return "set session sql_mode='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION'";
     }
 
 }
