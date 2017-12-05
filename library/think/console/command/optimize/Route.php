23db36a918e5d2f9fb904d47a97c69753dad6ef0
thinkphp <thinkphp@qq.com>
2017-12-05 09:29:28 +0800

    路由生成

--- library/think/console/command/optimize/Route.php
+++ library/think/console/command/optimize/Route.php
@@ -37,20 +37,56 @@ class Route extends Command
         Container::get('route')->setName([]);
         Container::get('config')->set('url_lazy_route', false);
         // 路由检测
-        $path  = Container::get('app')->getRoutePath();
+        $path = Container::get('app')->getRoutePath();
+
         $files = scandir($path);
-        foreach ($files as $file) {
-            if (strpos($file, '.php')) {
-                $filename = $path . DIRECTORY_SEPARATOR . $file;
-                // 导入路由配置
-                $rules = include $filename;
-                if (is_array($rules)) {
-                    Container::get('route')->import($rules);
+        if (!empty($files)) {
+            foreach ($files as $file) {
+                if (strpos($file, '.php')) {
+                    $filename = $path . DIRECTORY_SEPARATOR . $file;
+                    // 导入路由配置
+                    $rules = include $filename;
+                    if (is_array($rules)) {
+                        Container::get('route')->import($rules);
+                    }
                 }
             }
+        } else {
+            $controllers = scandir(Container::get('app')->getAppPath() . 'controller');
+            $route       = '';
+            foreach ($controllers as $controller) {
+                $route .= $this->getControllerRoute($controller);
+            }
+            $filename = Container::get('app')->getRuntimePath() . '_route.php';
+            file_put_contents($filename, $route);
+            include $filename;
         }
         $content = '<?php ' . PHP_EOL . 'return ';
         $content .= var_export(Container::get('route')->getName(), true) . ';';
         return $content;
+    }
+
+    protected function getControllerRoute($class)
+    {
+        $class   = new \ReflectionClass($class);
+        $methods = $class->getMethods(\ReflectionMethod::IS_PUBLIC);
+        $route   = [];
+        foreach ($methods as $method) {
+            $route[] = $this->getRouteComment($method);
+        }
+        return implode('', $route);
+    }
+
+    protected function getRouteComment($reflectMethod)
+    {
+        $comment = substr($reflectMethod->getDocComment(), 3, -2);
+        if (strpos($comment, '@route')) {
+            $comment = explode("\n", (strstr(trim($comment), '@route')));
+            $comment = array_map(function ($item) {return trim(trim($item), '*');}, $comment);
+            $key     = array_search('', $comment);
+            $comment = implode('', array_slice($comment, 0, $key));
+            dump($comment);
+        }
+
     }
 }
