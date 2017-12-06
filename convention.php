462b08ff1a267ddb1ce78624db5729a212915871
thinkphp <thinkphp@qq.com>
2017-12-06 14:32:46 +0800

    默认关闭注解路由自动生成 改进一个7.2的兼容性问题

--- convention.php
+++ convention.php
@@ -92,7 +92,7 @@ return [
         // 路由是否完全匹配
         'route_complete_match'   => false,
         // 使用注解路由
-        'route_annotation'       => true,
+        'route_annotation'       => false,
         // 域名根，如thinkphp.cn
         'url_domain_root'        => '',
         // 是否自动转换URL中的控制器和操作名
