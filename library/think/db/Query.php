47a74b531ee19353d5280a4c06d264c8aaf54ebe
thinkphp <thinkphp@qq.com>
2017-12-07 14:26:28 +0800

    修正join

--- library/think/db/Query.php
+++ library/think/db/Query.php
@@ -789,7 +789,8 @@ class Query
     {
         // 传入的表名为数组
         if (is_array($join)) {
-            list($table, $alias) = $join;
+            $table = key($join);
+            $alias = current($join);
         } else {
             $join = trim($join);
 
