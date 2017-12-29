ea746474e3fecd235d35c1fa94e6823e858136fb
thinkphp <thinkphp@qq.com>
2017-12-29 16:03:01 +0800

    修正模型的查询缓存自动更新

--- library/think/Model.php
+++ library/think/Model.php
@@ -461,7 +461,7 @@ abstract class Model implements \JsonSerializable, \ArrayAccess
 
         foreach ((array) $pk as $key) {
             if (isset($data[$key])) {
-                $array[] = [$key, '=', $data[$key]];
+                $array[$key] = [$key, '=', $data[$key]];
                 unset($data[$key]);
             }
         }
