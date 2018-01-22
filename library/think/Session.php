697425302a03b64f9ec1c61e072f1ba2c7b43237
thinkphp <thinkphp@qq.com>
2018-01-22 13:52:00 +0800

    session类增加use_lock配置参数设置是否启用锁机制 默认关闭

--- library/think/Session.php
+++ library/think/Session.php
@@ -46,6 +46,12 @@ class Session
     protected $lockTimeout = 3;
 
     /**
+     * 是否启用锁机制
+     * @var bool
+     */
+    protected $lock = false;
+
+    /**
      * 设置或者获取session作用域（前缀）
      * @access public
      * @param  string $prefix
@@ -90,6 +96,10 @@ class Session
 
         if (isset($config['prefix'])) {
             $this->prefix = $config['prefix'];
+        }
+
+        if (isset($config['use_lock'])) {
+            $this->lock = $config['use_lock'];
         }
 
         if (isset($config['var_session_id']) && isset($_REQUEST[$config['var_session_id']])) {
@@ -180,7 +190,7 @@ class Session
      */
     public function set($name, $value, $prefix = null)
     {
-        $this->lock(); // lock必须先于 $this->boot()
+        $this->lock();
 
         empty($this->init) && $this->boot();
 
@@ -200,7 +210,6 @@ class Session
             $_SESSION[$name] = $value;
         }
 
-        $this->pause();
         $this->unlock();
     }
 
@@ -213,7 +222,7 @@ class Session
      */
     public function get($name = '', $prefix = null)
     {
-        $this->lock(); // lock必须先于 $this->boot()
+        $this->lock();
 
         empty($this->init) && $this->boot();
 
@@ -234,7 +243,6 @@ class Session
             }
         }
 
-        $this->pause();
         $this->unlock();
 
         return $value;
@@ -247,6 +255,7 @@ class Session
     {
         // 不在 init 方法中实例化lockDriver，是因为 init 方法不一定先于 set 或 get 方法调用
         $config = Container::get('config')->pull('session');
+
         if (!empty($config['type']) && isset($config['use_lock']) && $config['use_lock']) {
             // 读取session驱动
             $class = false !== strpos($config['type'], '\\') ? $config['type'] : '\\think\\session\\driver\\' . ucwords($config['type']);
@@ -256,10 +265,12 @@ class Session
                 $this->lockDriver = new $class($config);
             }
         }
+
         // 通过cookie获得session_id
         if (isset($config['name']) && $config['name']) {
             $this->sessKey = $config['name'];
         }
+
         if (isset($config['lock_timeout']) && $config['lock_timeout'] > 0) {
             $this->lockTimeout = $config['lock_timeout'];
         }
@@ -272,12 +283,17 @@ class Session
      */
     protected function lock()
     {
+        if (empty($this->lock)) {
+            return;
+        }
+
         $this->initDriver();
 
         if (null !== $this->lockDriver && method_exists($this->lockDriver, 'lock')) {
             $t = time();
             // 使用 session_id 作为互斥条件，即只对同一 session_id 的会话互斥。第一次请求没有 session_id
             $sessID = isset($_COOKIE[$this->sessKey]) ? $_COOKIE[$this->sessKey] : '';
+
             do {
                 if (time() - $t > $this->lockTimeout) {
                     $this->unlock();
@@ -293,6 +309,12 @@ class Session
      */
     protected function unlock()
     {
+        if (empty($this->lock)) {
+            return;
+        }
+
+        $this->pause();
+
         if ($this->lockDriver && method_exists($this->lockDriver, 'unlock')) {
             $sessID = isset($_COOKIE[$this->sessKey]) ? $_COOKIE[$this->sessKey] : '';
             $this->lockDriver->unlock($sessID);
@@ -344,6 +366,10 @@ class Session
      */
     public function flush()
     {
+        if (!$this->init) {
+            return;
+        }
+
         $item = $this->get('__flash__');
 
         if (!empty($item)) {
@@ -367,6 +393,7 @@ class Session
     public function delete($name, $prefix = null)
     {
         empty($this->init) && $this->boot();
+
         $prefix = !is_null($prefix) ? $prefix : $this->prefix;
 
         if (is_array($name)) {
@@ -499,7 +526,6 @@ class Session
     {
         // 暂停session
         session_write_close();
-
         $this->init = false;
     }
 }
