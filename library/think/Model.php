4f1aeb6048bade418963488864e9add51ce66672
thinkphp <thinkphp@qq.com>
2018-01-31 15:21:19 +0800

    模型增加getOrFail方法

--- library/think/Model.php
+++ library/think/Model.php
@@ -772,10 +772,11 @@ abstract class Model implements \JsonSerializable, \ArrayAccess
      * @param  mixed     $data  主键值或者查询条件（闭包）
      * @param  mixed     $with  关联预查询
      * @param  bool      $cache 是否缓存
+     * @param  bool      $failException 是否抛出异常
      * @return static|null
      * @throws exception\DbException
      */
-    public static function get($data, $with = [], $cache = false)
+    public static function get($data, $with = [], $cache = false, $failException = false)
     {
         if (is_null($data)) {
             return;
@@ -788,7 +789,21 @@ abstract class Model implements \JsonSerializable, \ArrayAccess
 
         $query = static::parseQuery($data, $with, $cache);
 
-        return $query->find($data);
+        return $query->failException($failException)->find($data);
+    }
+
+    /**
+     * 查找单条记录
+     * @access public
+     * @param  mixed     $data  主键值或者查询条件（闭包）
+     * @param  mixed     $with  关联预查询
+     * @param  bool      $cache 是否缓存
+     * @return static|null
+     * @throws exception\DbException
+     */
+    public static function getOrFail($data, $with = [], $cache = false)
+    {
+        return self::get($data, $with, $cache, true);
     }
 
     /**
