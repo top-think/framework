158aa4302844c18ec8b962c1326cd7412125abd2
thinkphp <thinkphp@qq.com>
2018-03-30 16:07:15 +0800

    改进Request类ip方法 支持代理设置

--- library/think/Request.php
+++ library/think/Request.php
@@ -1273,7 +1273,11 @@ class Request
             return $ip[$type];
         }
 
-        if ($adv) {
+        $httpAgentIp = Config::get('http_agent_ip');
+
+        if ($httpAgentIp && isset($_SERVER[$httpAgentIp])) {
+            $ip = $_SERVER[$httpAgentIp];
+        } elseif ($adv) {
             if (isset($_SERVER['HTTP_X_FORWARDED_FOR'])) {
                 $arr = explode(',', $_SERVER['HTTP_X_FORWARDED_FOR']);
                 $pos = array_search('unknown', $arr);
