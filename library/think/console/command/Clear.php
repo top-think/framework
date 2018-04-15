c2855576b83709960b0ab639b402561158a9db3b
thinkphp <thinkphp@qq.com>
2018-04-15 12:32:41 +0800

    改进think clear指令 支持 -c -l -r 选项

--- library/think/console/command/Clear.php
+++ library/think/console/command/Clear.php
@@ -24,22 +24,40 @@ class Clear extends Command
         $this
             ->setName('clear')
             ->addOption('path', 'd', Option::VALUE_OPTIONAL, 'path to clear', null)
+            ->addOption('cache', 'c', Option::VALUE_NONE, 'clear cache file')
+            ->addOption('log', 'l', Option::VALUE_NONE, 'clear log file')
+            ->addOption('dir', 'r', Option::VALUE_NONE, 'clear empty dir')
             ->setDescription('Clear runtime file');
     }
 
     protected function execute(Input $input, Output $output)
     {
-        $path  = $input->getOption('path') ?: App::getRuntimePath();
-        $files = scandir($path);
-        if ($files) {
-            foreach ($files as $file) {
-                if ('.' != $file && '..' != $file && is_dir($path . $file)) {
-                    array_map('unlink', glob($path . $file . DIRECTORY_SEPARATOR . '*.*'));
-                } elseif ('.gitignore' != $file && is_file($path . $file)) {
-                    unlink($path . $file);
+        if ($input->getOption('cache')) {
+            $path = App::getRuntimePath() . 'cache';
+        } elseif ($input->getOption('log')) {
+            $path = App::getRuntimePath() . 'log';
+        } else {
+            $path = $input->getOption('path') ?: App::getRuntimePath();
+        }
+
+        $rmdir = $input->getOption('dir') ? true : false;
+        $this->clear(rtrim($path, DIRECTORY_SEPARATOR) . DIRECTORY_SEPARATOR, $rmdir);
+        $output->writeln("<info>Clear Successed</info>");
+    }
+
+    protected function clear($path, $rmdir)
+    {
+        $files = is_dir($path) ? scandir($path) : [];
+
+        foreach ($files as $file) {
+            if ('.' != $file && '..' != $file && is_dir($path . $file)) {
+                array_map('unlink', glob($path . $file . DIRECTORY_SEPARATOR . '*.*'));
+                if ($rmdir) {
+                    rmdir($path . $file);
                 }
+            } elseif ('.gitignore' != $file && is_file($path . $file)) {
+                unlink($path . $file);
             }
         }
-        $output->writeln("<info>Clear Successed</info>");
     }
 }
