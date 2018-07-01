8e92f28c89ec328c8dc9771b2c803e678706cc31
Tekin Tian <tekin@vip.qq.com>
2018-07-01 17:12:30 +0800

    资源路由生成错误修复
    276行  Route::resourece   单词"resourece"拼写错误， 正确的应该是 resource

--- library/think/Build.php
+++ library/think/Build.php
@@ -273,7 +273,7 @@ class Build
         if (false !== strpos($comment, '@route(')) {
             $comment = $this->parseRouteComment($comment);
             $route   = $module . '/' . $controller;
-            $comment = preg_replace('/route\(\s?([\'\"][\-\_\/\:\<\>\?\$\[\]\w]+[\'\"])\s?\)/is', 'Route::resourece(\1,\'' . $route . '\')', $comment);
+            $comment = preg_replace('/route\(\s?([\'\"][\-\_\/\:\<\>\?\$\[\]\w]+[\'\"])\s?\)/is', 'Route::resource(\1,\'' . $route . '\')', $comment);
             $content .= PHP_EOL . $comment;
         } elseif (false !== strpos($comment, '@alias(')) {
             $comment = $this->parseRouteComment($comment, '@alias(');
