886e137dcb2409a52f1f8f07a146ea8fbd17181b
thinkphp <thinkphp@qq.com>
2018-07-04 14:40:40 +0800

    容器类增加IteratorAggregate和Countable接口

--- library/think/Container.php
+++ library/think/Container.php
@@ -11,15 +11,19 @@
 
 namespace think;
 
+use ArrayAccess;
+use ArrayIterator;
 use Closure;
+use Countable;
 use InvalidArgumentException;
+use IteratorAggregate;
 use ReflectionClass;
 use ReflectionException;
 use ReflectionFunction;
 use ReflectionMethod;
 use think\exception\ClassNotFoundException;
 
-class Container implements \ArrayAccess
+class Container implements ArrayAccess, IteratorAggregate, Countable
 {
     /**
      * 容器对象实例
@@ -152,6 +156,9 @@ class Container implements \ArrayAccess
         } elseif ($concrete instanceof Closure) {
             $this->bind[$abstract] = $concrete;
         } elseif (is_object($concrete)) {
+            if (isset($this->bind[$abstract])) {
+                $abstract = $this->bind[$abstract];
+            }
             $this->instances[$abstract] = $concrete;
         } else {
             $this->bind[$abstract] = $concrete;
@@ -276,6 +283,16 @@ class Container implements \ArrayAccess
                 unset($this->instances[$name]);
             }
         }
+    }
+
+    /**
+     * 获取容器中的对象实例
+     * @access public
+     * @return array
+     */
+    public function all()
+    {
+        return $this->instances;
     }
 
     /**
@@ -501,5 +518,17 @@ class Container implements \ArrayAccess
     public function offsetUnset($key)
     {
         $this->__unset($key);
+    }
+
+    //Countable
+    public function count()
+    {
+        return count($this->instances);
+    }
+
+    //IteratorAggregate
+    public function getIterator()
+    {
+        return new ArrayIterator($this->instances);
     }
 }
