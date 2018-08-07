6e87982cf0bef555ea5987aef18ff8767f44a2bd
thinkphp <thinkphp@qq.com>
2018-08-07 19:19:32 +0800

    注释调整

--- library/think/facade/Config.php
+++ library/think/facade/Config.php
@@ -18,7 +18,7 @@ use think\Facade;
  * @mixin \think\Config
  * @method bool has(string $name) static 检测配置是否存在
  * @method array pull(string $name) static 获取一级配置
- * @method mixed get(string $name) static 获取配置参数
+ * @method mixed get(string $name,mixed $default = null) static 获取配置参数
  * @method mixed set(string $name, mixed $value = null) static 设置配置参数
  * @method array reset(string $prefix ='') static 重置配置参数
  */
