12381b47fb506a58a2fa81b7c644997ac0dd37b9
thinkphp <thinkphp@qq.com>
2018-08-08 16:21:46 +0800

    改进formatDateTime方法判断

--- library/think/model/concern/TimeStamp.php
+++ library/think/model/concern/TimeStamp.php
@@ -50,6 +50,10 @@ trait TimeStamp
      */
     protected function formatDateTime($time, $format, $timestamp = false)
     {
+        if (empty($time)) {
+            return;
+        }
+
         if (false !== strpos($format, '\\')) {
             $time = new $format($time);
         } elseif (!$timestamp && false !== $format) {
