4dcf45addbcf87b7f08d2e5db14de50df538e428
llqhz <llqhz@qq.com>
2018-08-28 18:16:13 +0800

    前置控制器操作忽略大小写

--- library/think/Controller.php
+++ library/think/Controller.php
@@ -99,23 +99,25 @@ class Controller
     /**
      * 前置操作
      * @access protected
-     * @param  string $method  前置操作方法名
+     * @param  string $method  前置操作方法名(忽略大小写)
      * @param  array  $options 调用参数 ['only'=>[...]] 或者['except'=>[...]]
      */
     protected function beforeAction($method, $options = [])
     {
         if (isset($options['only'])) {
-            if (is_string($options['only'])) {
-                $options['only'] = explode(',', $options['only']);
+            if (is_array($options['only'])) {
+                $options['only'] = implode(',', $options['only']);
             }
-            if (!in_array($this->request->action(), $options['only'])) {
+            $options['only'] = explode(',', strtolower($options['only']));
+            if (!in_array(strtolower($this->request->action()), $options['only'])) {
                 return;
             }
         } elseif (isset($options['except'])) {
-            if (is_string($options['except'])) {
-                $options['except'] = explode(',', $options['except']);
+            if (is_array($options['except'])) {
+                $options['except'] = implode(',', $options['except']);
             }
-            if (in_array($this->request->action(), $options['except'])) {
+            $options['except'] = explode(',', strtolower($options['except']));
+            if (in_array(strtolower($this->request->action()), $options['except'])) {
                 return;
             }
         }
