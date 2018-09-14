5cbfa1c50c8ec1736e77e9b75d645a63504400ab
opensmarty <opensmarty@163.com>
2018-09-14 16:23:46 +0800

    优化getModuleListClass函数

--- library/think/App.php
+++ library/think/App.php
@@ -853,16 +853,16 @@ class App extends Container
     /**
      * 获取模块下的类名
      * @access protected
-     * @param  string  $class   类名
-     * @param  string  $module  模块名
+     * @param  string $class 类名
+     * @param  string $module 模块名
      * @return object  返回应用的类名
      */
     protected function getModuleListClass($class, $module)
     {
-        foreach ($this->moduleList as $_module) {
-            $_class = str_replace('\\' . $module . '\\', '\\' . $_module . '\\', $class);
-            if (class_exists($_class)) {
-                return $_class;
+        foreach ($this->moduleList as $moduleName) {
+            $className = str_replace('\\' . $module . '\\', '\\' . $moduleName . '\\', $class);
+            if (class_exists($className)) {
+                return $className;
             }
         }
     }
@@ -880,19 +880,19 @@ class App extends Container
     public function create($name, $layer, $appendSuffix = false, $common = 'common')
     {
         $guid = $name . $layer;
-        
+
         if ($this->__isset($guid)) {
             return $this->__get($guid);
         }
-        
+
         list($module, $class) = $this->parseModuleAndClass($name, $layer, $appendSuffix);
-        
-        $_class = $this->getModuleListClass($class, $module);
-        
+
+        $className = $this->getModuleListClass($class, $module);
+
         if (class_exists($class)) {
             $object = $this->__get($class);
-        } elseif (class_exists($_class)) {
-            $class  = $_class;
+        } elseif (class_exists($className)) {
+            $class  = $className;
             $object = $this->__get($class);
         } else {
             $class = str_replace('\\' . $module . '\\', '\\' . $common . '\\', $class);
@@ -902,9 +902,9 @@ class App extends Container
                 throw new ClassNotFoundException('class not exists:' . $class, $class);
             }
         }
-        
+
         $this->__set($guid, $class);
-        
+
         return $object;
     }
 
