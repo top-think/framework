211fe20c1b84b8125bf097e89bb4bf70f7a2ada3
王雪兵 <406964108@qq.com>
2018-09-21 10:52:37 +0800

    解决直接使用new xxModel()或静态调用时使用alias方法不生效的问题

--- library/think/db/Query.php
+++ library/think/db/Query.php
@@ -1672,6 +1672,9 @@ class Query
                 }
             } else {
                 $table = $this->getTable();
+                if (false !== strpos($table, '__')) {
+                    $table = $this->parseSqlTable($table);
+                }
             }
 
             $this->options['alias'][$table] = $alias;
