1f94eacbdbde34eff5804c9d03980cc161c1d363
thinkphp <thinkphp@qq.com>
2018-11-05 18:57:03 +0800

    修正console调试驱动

--- src/library/think/debug/Console.php
+++ src/library/think/debug/Console.php
@@ -12,7 +12,6 @@
 namespace think\debug;
 
 use think\Container;
-use think\Db;
 use think\Response;
 
 /**
@@ -64,7 +63,7 @@ class Console
         $base = [
             '请求信息' => date('Y-m-d H:i:s', $_SERVER['REQUEST_TIME']) . ' ' . $uri,
             '运行时间' => number_format($runtime, 6) . 's [ 吞吐率：' . $reqs . 'req/s ] 内存消耗：' . $mem . 'kb 文件加载：' . count(get_included_files()),
-            '查询信息' => Db::$queryTimes . ' queries ' . Db::$executeTimes . ' writes ',
+            '查询信息' => Container::get('db')->getQueryTimes() . ' queries ' . Container::get('db')->getExecuteTimes() . ' writes ',
             '缓存信息' => Container::get('cache')->getReadTimes() . ' reads,' . Container::get('cache')->getWriteTimes() . ' writes',
         ];
 
