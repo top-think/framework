8edf352c67e941a696206939514063747bab619a
thinkphp <thinkphp@qq.com>
2018-11-07 12:37:46 +0800

    修正注释

--- src/think/facade/View.php
+++ src/think/facade/View.php
@@ -23,8 +23,8 @@ use think\Facade;
  * @method \think\View exists(mixed $name) static 检查模板是否存在
  * @method \think\View filter(Callable $filter) static 视图内容过滤
  * @method \think\View engine(mixed $engine = []) static 设置当前模板解析的引擎
- * @method string fetch(string $template = '', array $vars = [], array $replace = [], array $config = [], bool $renderContent = false) static 解析和获取模板内容
- * @method string display(string $content = '', array $vars = [], array $replace = [], array $config = []) static 渲染内容输出
+ * @method string fetch(string $template = '', array $vars = [], array $config = [], bool $renderContent = false) static 解析和获取模板内容
+ * @method string display(string $content = '', array $vars = [], array $config = []) static 渲染内容输出
  */
 class View extends Facade
 {
