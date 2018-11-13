c5052c8b813f71df5ab6d5d8371bc58c5545a233
lineofsky2017 <34702497+lineofsky2017@users.noreply.github.com>
2018-11-13 21:43:57 +0800

    调整对int型转换成float型的处理，跟bindValue保持一致

--- library/think/db/Connection.php
+++ library/think/db/Connection.php
@@ -1463,10 +1463,12 @@ abstract class Connection
             $value = is_array($val) ? $val[0] : $val;
             $type  = is_array($val) ? $val[1] : PDO::PARAM_STR;
 
-            if (PDO::PARAM_INT == $type || self::PARAM_FLOAT == $type) {
+            if (self::PARAM_FLOAT == $type) {
                 $value = (float) $value;
             } elseif (PDO::PARAM_STR == $type) {
                 $value = '\'' . addslashes($value) . '\'';
+            } elseif(PDO::PARAM_INT == $type && '' === $value) {
+                $value = 0;
             }
 
             // 判断占位符
