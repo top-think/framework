eb84e9979812a4cb867ea6eb23934b90ef424fd3
thinkphp <thinkphp@qq.com>
2018-11-20 15:50:49 +0800

    改进软删除的destroy方法

--- library/think/model/concern/SoftDelete.php
+++ library/think/model/concern/SoftDelete.php
@@ -140,7 +140,7 @@ trait SoftDelete
     public static function destroy($data, $force = false)
     {
         // 包含软删除数据
-        $query = self::withTrashed();
+        $query = (new static())->db(false);
 
         if (is_array($data) && key($data) !== 0) {
             $query->where($data);
