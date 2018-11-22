f60da098b64ec747bd21594f21529c5d231e7c1f
HongWeipeng <hongweichen8888@sina.com>
2018-11-22 15:57:33 +0800

    修复 array_walk_recursive 在低于PHP7.1消耗内部指针问题

--- library/think/Request.php
+++ library/think/Request.php
@@ -1315,6 +1315,21 @@ class Request
     }
 
     /**
+     * 递归重置数组指针
+     * @access public
+     * @param array $data 数据源
+     * @return void
+     */
+    public function arrayReset(array &$data) {
+        foreach ($data as &$value) {
+            if (is_array($value)) {
+                $this->arrayReset($value);
+            }
+        }
+        reset($data);
+    }
+
+    /**
      * 获取变量 支持过滤和默认值
      * @access public
      * @param  array         $data 数据源
@@ -1353,7 +1368,10 @@ class Request
 
         if (is_array($data)) {
             array_walk_recursive($data, [$this, 'filterValue'], $filter);
-            reset($data);
+            if (version_compare(PHP_VERSION, '7.1.0', '<')) {
+                // 恢复PHP版本低于 7.1 时 array_walk_recursive 中消耗的内部指针
+                $this->arrayReset($data);
+            }
         } else {
             $this->filterValue($data, $name, $filter);
         }
