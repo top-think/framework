3ac714b7c63138b97736a8bfc2bae688b2c499cf
thinkphp <thinkphp@qq.com>
2018-12-06 15:48:54 +0800

    统一助手函数与\think\response\Download函数文件过期时间

--- helper.php
+++ helper.php
@@ -313,7 +313,7 @@ if (!function_exists('download')) {
      * @param integer $expire 有效期（秒）
      * @return \think\response\Download
      */
-    function download($filename, $name = '', $content = false, $expire = 180, $openinBrower = false)
+    function download($filename, $name = '', $content = false, $expire = 360, $openinBrower = false)
     {
         return Response::create($filename, 'download')->name($name)->isContent($content)->expire($expire)->openinBrower($openinBrower);
     }
