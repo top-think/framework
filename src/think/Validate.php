bbf0cba433ca81b0da937195f3d5800fa16ac6e9
thinkphp <thinkphp@qq.com>
2018-12-20 16:45:37 +0800

    改进unique验证方法

--- src/think/Validate.php
+++ src/think/Validate.php
@@ -996,10 +996,14 @@ class Validate
             // 支持多个字段验证
             $fields = explode('^', $key);
             foreach ($fields as $key) {
-                $map[] = [$key, '=', $data[$key]];
+                if (isset($data[$field])) {
+                    $map[] = [$key, '=', $data[$key]];
+                }
             }
-        } else {
+        } elseif (isset($data[$field])) {
             $map[] = [$key, '=', $data[$field]];
+        } else {
+            $map = [];
         }
 
         $pk = !empty($rule[3]) ? $rule[3] : $db->getPk();
