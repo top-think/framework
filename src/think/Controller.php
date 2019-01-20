b605ea4c5b887fe3e640029171716567ff12a583
thinkphp <thinkphp@qq.com>
2019-01-20 18:44:42 +0800

    视图类和模板引擎独立 使用topthink/think-view类库支持

--- src/think/Controller.php
+++ src/think/Controller.php
@@ -20,12 +20,6 @@ class Controller
     use Jump;
 
     /**
-     * 视图类实例
-     * @var \think\View
-     */
-    protected $view;
-
-    /**
      * Request实例
      * @var \think\Request
      */
@@ -64,7 +58,6 @@ class Controller
     {
         $this->app     = $app;
         $this->request = $this->app['request'];
-        $this->view    = $this->app['view'];
 
         // 控制器初始化
         $this->initialize();
@@ -92,67 +85,6 @@ class Controller
     // 初始化
     protected function initialize()
     {}
-
-    /**
-     * 加载模板输出
-     * @access protected
-     * @param  string $template 模板文件名
-     * @return mixed
-     */
-    protected function fetch(string $template = '')
-    {
-        return $this->view->fetch($template);
-    }
-
-    /**
-     * 渲染内容输出
-     * @access protected
-     * @param  string $content 模板内容
-     * @return mixed
-     */
-    protected function display(string $content = '')
-    {
-        return $this->view->display($content);
-    }
-
-    /**
-     * 模板变量赋值
-     * @access protected
-     * @param  array $vars 模板变量
-     * @return $this
-     */
-    protected function assign(array $vars)
-    {
-        $this->view->assign($vars);
-
-        return $this;
-    }
-
-    /**
-     * 视图过滤
-     * @access protected
-     * @param  Callable $filter 过滤方法或闭包
-     * @return $this
-     */
-    protected function filter(callable $filter)
-    {
-        $this->view->filter($filter);
-
-        return $this;
-    }
-
-    /**
-     * 初始化模板引擎
-     * @access protected
-     * @param  array|string $engine 引擎参数
-     * @return $this
-     */
-    protected function engine($engine)
-    {
-        $this->view->engine($engine);
-
-        return $this;
-    }
 
     /**
      * 设置验证失败后是否抛出异常
