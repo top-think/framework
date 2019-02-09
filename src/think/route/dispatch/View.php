6515f0c2e3010b98978b9dd9bf4f57e9b9ae6c52
thinkphp <thinkphp@qq.com>
2019-02-09 18:58:56 +0800

    改进

--- src/think/route/dispatch/View.php
+++ src/think/route/dispatch/View.php
@@ -20,8 +20,6 @@ class View extends Dispatch
     public function exec()
     {
         // 渲染模板输出
-        $vars = array_merge($this->request->param(), $this->param);
-
-        return Response::create($this->dispatch, 'view')->assign($vars);
+        return Response::create($this->dispatch, 'view')->assign($this->param);
     }
 }
