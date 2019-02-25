3ba6f8c476e33a85e3fc6ee2a688855ada7f780d
thinkphp <thinkphp@qq.com>
2019-02-25 14:35:57 +0800

    改进关联属性的visible

--- library/think/model/concern/Conversion.php
+++ library/think/model/concern/Conversion.php
@@ -140,7 +140,9 @@ trait Conversion
         // 过滤属性
         if (!empty($this->visible)) {
             $array = $this->parseAttr($this->visible, $visible);
-            $data  = array_intersect_key($data, array_flip($array));
+            if (!empty($array)) {
+                $data = array_intersect_key($data, array_flip($array));
+            }
         } elseif (!empty($this->hidden)) {
             $array = $this->parseAttr($this->hidden, $hidden, false);
             $data  = array_diff_key($data, array_flip($array));
@@ -253,28 +255,18 @@ trait Conversion
      * @access protected
      * @param  array $attrs  属性
      * @param  array $result 结果集
-     * @param  bool  $visible
      * @return array
      */
-    protected function parseAttr($attrs, &$result, $visible = true)
+    protected function parseAttr($attrs, &$result)
     {
         $array = [];
 
         foreach ($attrs as $key => $val) {
             if (is_array($val)) {
-                if ($visible) {
-                    $array[] = $key;
-                }
-
                 $result[$key] = $val;
             } elseif (strpos($val, '.')) {
                 list($key, $name) = explode('.', $val);
-
-                if ($visible) {
-                    $array[] = $key;
-                }
-
-                $result[$key][] = $name;
+                $result[$key][]   = $name;
             } else {
                 $array[] = $val;
             }
