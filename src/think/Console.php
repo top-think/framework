e48ed6c22163438dca1d43eb9cef74f05c06ac4a
thinkphp <thinkphp@qq.com>
2019-04-09 17:06:06 +0800

    增加make:service指令

--- src/think/Console.php
+++ src/think/Console.php
@@ -25,6 +25,7 @@ use think\console\command\make\Event;
 use think\console\command\make\Listener;
 use think\console\command\make\Middleware;
 use think\console\command\make\Model;
+use think\console\command\make\Service;
 use think\console\command\make\Subscribe;
 use think\console\command\make\Validate;
 use think\console\command\optimize\Config;
@@ -74,6 +75,7 @@ class Console
         'make:validate'    => Validate::class,
         'make:event'       => Event::class,
         'make:listener'    => Listener::class,
+        'make:service'     => Service::class,
         'make:subscribe'   => Subscribe::class,
         'optimize:config'  => Config::class,
         'optimize:schema'  => Schema::class,
