1ee4373a67be3118289702c5936b28626726a7bb
thinkphp <thinkphp@qq.com>
2019-04-19 10:24:37 +0800

    改进异常处理

--- src/think/initializer/Error.php
+++ src/think/initializer/Error.php
@@ -52,7 +52,7 @@ class Error
         if ($this->app->runningInConsole()) {
             $handler->renderForConsole(new ConsoleOutput, $e);
         } else {
-            $handler->render($this->app->request, $e)->send();
+            $handler->render($this->app->request, $e)->setCookie($this->app->cookie)->send();
         }
     }
 
