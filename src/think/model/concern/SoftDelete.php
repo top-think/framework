ce7d7b36da6fd933158de7345cc0061da710f84c
thinkphp <thinkphp@qq.com>
2019-04-23 10:15:10 +0800

    修正软删除

--- src/think/model/concern/SoftDelete.php
+++ src/think/model/concern/SoftDelete.php
@@ -239,7 +239,7 @@ trait SoftDelete
         $field = $this->getDeleteTimeField(true);
 
         if ($field) {
-            $query->useSoftDelete($field, $this->defaultSoftDelete);
+            $query->useSoftDelete($field, $this->getWithTrashedExp());
         }
     }
 }
