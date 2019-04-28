2f32374eda742e281ddf6667fe849a5d1fae614f
thinkphp <thinkphp@qq.com>
2019-04-28 16:12:18 +0800

    增加error、success和result助手函数

--- src/helper.php
+++ src/helper.php
@@ -277,6 +277,43 @@ if (!function_exists('env')) {
     }
 }
 
+if (!function_exists('error')) {
+    /**
+     * 操作错误跳转的快捷方法
+     * @param  mixed   $msg 提示信息
+     * @param  string  $url 跳转的URL地址
+     * @param  mixed   $data 返回的数据
+     * @param  integer $wait 跳转等待时间
+     * @param  array   $header 发送的Header信息
+     * @return void
+     */
+    function error($msg = '', string $url = null, $data = '', int $wait = 3, array $header = []): void
+    {
+        if (is_null($url)) {
+            $url = request()->isAjax() ? '' : 'javascript:history.back(-1);';
+        } elseif ($url) {
+            $url = (strpos($url, '://') || 0 === strpos($url, '/')) ? $url : app('route')->buildUrl($url);
+        }
+
+        $result = [
+            'code' => 0,
+            'msg'  => $msg,
+            'data' => $data,
+            'url'  => $url,
+            'wait' => $wait,
+        ];
+
+        $type = (request()->isJson() || request()->isAjax()) ? 'json' : 'html';
+        if ('html' == strtolower($type)) {
+            $type = 'jump';
+        }
+
+        $response = Response::create($result, $type)->header($header)->options(['jump_template' => app('config')->get('app.dispatch_error_tmpl')]);
+
+        throw new HttpResponseException($response);
+    }
+}
+
 if (!function_exists('event')) {
     /**
      * 触发事件
@@ -478,6 +515,32 @@ if (!function_exists('response')) {
     }
 }
 
+if (!function_exists('result')) {
+    /**
+     * 返回封装后的API数据到客户端
+     * @param  mixed   $data 要返回的数据
+     * @param  integer $code 返回的code
+     * @param  mixed   $msg 提示信息
+     * @param  string  $type 返回数据格式
+     * @param  array   $header 发送的Header信息
+     * @return void
+     */
+    function result($data, int $code = 0, $msg = '', string $type = '', array $header = []): void
+    {
+        $result = [
+            'code' => $code,
+            'msg'  => $msg,
+            'time' => time(),
+            'data' => $data,
+        ];
+
+        $type     = $type ?: 'json';
+        $response = Response::create($result, $type)->header($header);
+
+        throw new HttpResponseException($response);
+    }
+}
+
 if (!function_exists('route')) {
     /**
      * 路由注册
@@ -514,6 +577,44 @@ if (!function_exists('session')) {
             // 设置
             Session::set($name, $value);
         }
+    }
+}
+
+if (!function_exists('success')) {
+    /**
+     * 操作成功跳转的快捷方法
+     * @param  mixed     $msg 提示信息
+     * @param  string    $url 跳转的URL地址
+     * @param  mixed     $data 返回的数据
+     * @param  integer   $wait 跳转等待时间
+     * @param  array     $header 发送的Header信息
+     * @return void
+     */
+    function success($msg = '', string $url = null, $data = '', int $wait = 3, array $header = []): void
+    {
+        if (is_null($url) && isset($_SERVER["HTTP_REFERER"])) {
+            $url = $_SERVER["HTTP_REFERER"];
+        } elseif ($url) {
+            $url = (strpos($url, '://') || 0 === strpos($url, '/')) ? $url : app('route')->buildUrl($url);
+        }
+
+        $result = [
+            'code' => 1,
+            'msg'  => $msg,
+            'data' => $data,
+            'url'  => $url,
+            'wait' => $wait,
+        ];
+
+        $type = (request()->isJson() || request()->isAjax()) ? 'json' : 'html';
+        // 把跳转模板的渲染下沉，这样在 response_send 行为里通过getData()获得的数据是一致性的格式
+        if ('html' == strtolower($type)) {
+            $type = 'jump';
+        }
+
+        $response = Response::create($result, $type)->header($header)->options(['jump_template' => app('config')->get('app.dispatch_success_tmpl')]);
+
+        throw new HttpResponseException($response);
     }
 }
 
