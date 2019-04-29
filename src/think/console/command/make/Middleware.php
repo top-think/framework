44560bdfd948aefd24dcf1d097a3ce0e318a9300
thinkphp <thinkphp@qq.com>
2019-04-29 16:17:13 +0800

    改进make:Middleware生成的默认路径

--- src/think/console/command/make/Middleware.php
+++ src/think/console/command/make/Middleware.php
@@ -31,6 +31,6 @@ class Middleware extends Make
 
     protected function getNamespace(string $app): string
     {
-        return parent::getNamespace($app) . '\\middleware';
+        return 'app\\http\\middleware';
     }
 }
