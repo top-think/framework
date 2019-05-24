c434854cbb5e680ecaf4c7191ddb7e672059d286
thinkphp <thinkphp@qq.com>
2019-05-24 11:04:09 +0800

    改进跨越请求

--- library/think/route/Rule.php
+++ library/think/route/Rule.php
@@ -642,9 +642,7 @@ abstract class Rule
     protected function checkCrossDomain($request)
     {
         if (!empty($this->option['cross_domain'])) {
-
             $header = [
-                'Access-Control-Allow-Origin'      => $request->header('origin') ?: '',
                 'Access-Control-Allow-Credentials' => 'true',
                 'Access-Control-Allow-Methods'     => 'GET, POST, PATCH, PUT, DELETE',
                 'Access-Control-Allow-Headers'     => 'Authorization, Content-Type, If-Match, If-Modified-Since, If-None-Match, If-Unmodified-Since, X-Requested-With',
@@ -652,6 +650,16 @@ abstract class Rule
 
             if (!empty($this->option['header'])) {
                 $header = array_merge($header, $this->option['header']);
+            }
+
+            if (!isset($header['Access-Control-Allow-Origin'])) {
+                $httpOrigin = $request->header('origin');
+
+                if ($httpOrigin && strpos(config('cookie.domain'), $httpOrigin)) {
+                    $header['Access-Control-Allow-Origin'] = $httpOrigin;
+                } else {
+                    $header['Access-Control-Allow-Origin'] = '*';
+                }
             }
 
             $this->option['header'] = $header;
