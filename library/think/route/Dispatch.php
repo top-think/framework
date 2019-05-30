b88336192b97b73d61b55eac03c503065542030f
thinkphp <thinkphp@qq.com>
2019-05-30 20:03:36 +0800

    改进

--- library/think/route/Dispatch.php
+++ library/think/route/Dispatch.php
@@ -181,9 +181,10 @@ abstract class Dispatch
 
             $response = Response::create($data, $type);
         } else {
-            $data     = ob_get_clean();
-            $content  = false === $data ? '' : $data;
-            $status   = false === $data ? 204 : 200;
+            $data    = ob_get_clean();
+            $content = false === $data ? '' : $data;
+            $status  = '' === $content && $this->request->isJson() ? 204 : 200;
+
             $response = Response::create($content, '', $status);
         }
 
