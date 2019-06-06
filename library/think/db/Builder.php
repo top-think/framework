3a80ed2fee4ec5bab0d58d7eb765173856411189
thinkphp <thinkphp@qq.com>
2019-06-06 12:49:24 +0800

    改进parseLike查询

--- library/think/db/Builder.php
+++ library/think/db/Builder.php
@@ -407,7 +407,7 @@ abstract class Builder
             $jsonType = $query->getJsonFieldType($field);
             $bindType = $this->connection->getFieldBindType($jsonType);
         } else {
-            $bindType = isset($binds[$field]) ? $binds[$field] : PDO::PARAM_STR;
+            $bindType = isset($binds[$field]) && 'LIKE' != $exp ? $binds[$field] : PDO::PARAM_STR;
         }
 
         if (is_scalar($value) && !in_array($exp, ['EXP', 'NOT NULL', 'NULL', 'IN', 'NOT IN', 'BETWEEN', 'NOT BETWEEN']) && strpos($exp, 'TIME') === false) {
@@ -450,7 +450,7 @@ abstract class Builder
         // 模糊匹配
         if (is_array($value)) {
             foreach ($value as $item) {
-                $name    = $query->bind($item, $bindType);
+                $name    = $query->bind($item, PDO::PARAM_STR);
                 $array[] = $key . ' ' . $exp . ' :' . $name;
             }
 
