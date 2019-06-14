abd026ff3817cc3348508dfc57bfe87e0e427f8e
thinkphp <thinkphp@qq.com>
2019-06-14 12:21:14 +0800

    增加PDOConnection类

--- src/think/db/connector/Mysql.php
+++ src/think/db/connector/Mysql.php
@@ -13,13 +13,64 @@ declare (strict_types = 1);
 namespace think\db\connector;
 
 use PDO;
-use think\db\Connection;
+use think\db\PDOConnection;
 
 /**
  * mysql数据库驱动
  */
-class Mysql extends Connection
+class Mysql extends PDOConnection
 {
+
+    /**
+     * 数据库连接参数配置
+     * @var array
+     */
+    protected $config = [
+        // 数据库类型
+        'type'            => 'mysql',
+        // 服务器地址
+        'hostname'        => '127.0.0.1',
+        // 数据库名
+        'database'        => '',
+        // 用户名
+        'username'        => '',
+        // 密码
+        'password'        => '',
+        // 端口
+        'hostport'        => '',
+        // 连接dsn
+        'dsn'             => '',
+        // 数据库连接参数
+        'params'          => [],
+        // 数据库编码默认采用utf8
+        'charset'         => 'utf8',
+        // 数据库表前缀
+        'prefix'          => '',
+        // 数据库调试模式
+        'debug'           => false,
+        // 数据库部署方式:0 集中式(单一服务器),1 分布式(主从服务器)
+        'deploy'          => 0,
+        // 数据库读写是否分离 主从式有效
+        'rw_separate'     => false,
+        // 读写分离后 主服务器数量
+        'master_num'      => 1,
+        // 指定从服务器序号
+        'slave_no'        => '',
+        // 模型写入后自动读取主服务器
+        'read_master'     => false,
+        // 是否严格检查字段是否存在
+        'fields_strict'   => true,
+        // 是否需要进行SQL性能分析
+        'sql_explain'     => false,
+        // Builder类
+        'builder'         => '',
+        // Query类
+        'query'           => '',
+        // 是否需要断线重连
+        'break_reconnect' => false,
+        // 断线标识字符串
+        'break_match_str' => [],
+    ];
 
     /**
      * 解析pdo连接的dsn信息
