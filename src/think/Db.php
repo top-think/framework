e5d20067b99250c6d91237dd05976ba0aaad1ec3
thinkphp <thinkphp@qq.com>
2019-07-14 12:59:59 +0800

    改进Db类配置获取

--- src/think/Db.php
+++ src/think/Db.php
@@ -29,7 +29,7 @@ class Db extends DbManager
     public static function __make(Event $event, Config $config, Log $log, Cache $cache)
     {
         $db = new static();
-        $db->setConfig($config->get('database'));
+        $db->setConfig($config);
         $db->setEvent($event);
         $db->setLog($log);
         $db->setCache($cache);
@@ -47,10 +47,37 @@ class Db extends DbManager
     }
 
     /**
+     * 设置配置对象
+     * @access public
+     * @param Config $config 配置对象
+     * @return void
+     */
+    public function setConfig($config = []): void
+    {
+        $this->config = $config;
+    }
+
+    /**
+     * 获取配置参数
+     * @access public
+     * @param  string $config 配置参数
+     * @param  mixed  $default 默认值
+     * @return mixed
+     */
+    public function getConfig(string $name = '', $default = null)
+    {
+        if (!is_null($name)) {
+            return $this->config->get('database.' . $name, $default);
+        }
+
+        return $this->config->get('database');
+    }
+
+    /**
      * 设置Event对象
      * @param Event $event
      */
-    public function setEvent(Event $event)
+    public function setEvent(Event $event): void
     {
         $this->event = $event;
     }
