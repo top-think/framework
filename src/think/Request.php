014025eaad06ad61fd6dab451f891ebbbc915a13
thinkphp <thinkphp@qq.com>
2019-07-30 11:07:55 +0800

    改进Request类

--- src/think/Request.php
+++ src/think/Request.php
@@ -1138,7 +1138,7 @@ class Request
      * 获取上传的文件信息
      * @access public
      * @param  string $name 名称
-     * @return null|array|\think\file\UploadedFile
+     * @return null|array|UploadedFile
      */
     public function file(string $name = '')
     {
@@ -1404,7 +1404,7 @@ class Request
      * @param  array $filters 过滤方法+默认值
      * @return mixed
      */
-    private function filterValue(&$value, $key, $filters)
+    public function filterValue(&$value, $key, $filters)
     {
         $default = array_pop($filters);
 
@@ -1629,7 +1629,7 @@ class Request
             // 这时我们检查 REMOTE_ADDR 是不是指定的前端代理服务器之一
             // 如果是的话说明该 IP头 是由前端代理服务器设置的
             // 否则则是伪装的
-            if ($tempIP) {
+            if (!empty($tempIP)) {
                 $realIPBin = $this->ip2bin($this->realIP);
 
                 foreach ($proxyIp as $ip) {
@@ -1958,8 +1958,8 @@ class Request
     /**
      * 检查请求令牌
      * @access public
-     * @param  string $name 令牌名称
-     * @param  array  $data 表单数据
+     * @param  string $token 令牌名称
+     * @param  array  $data  表单数据
      * @return bool
      */
     public function checkToken(string $token = '__token__', array $data = []): bool
