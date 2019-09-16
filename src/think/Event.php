2656ee4e1fdec81e262c4d50e2462ca98e5fb291
yunwuxin <448901948@qq.com>
2019-09-16 11:48:29 +0800

    Event 单元测试

--- src/think/Event.php
+++ src/think/Event.php
@@ -28,12 +28,6 @@ class Event
     protected $listener = [];
 
     /**
-     * 观察者
-     * @var array
-     */
-    protected $observer = [];
-
-    /**
      * 事件别名
      * @var array
      */
@@ -65,7 +59,7 @@ class Event
     /**
      * 设置是否开启事件响应
      * @access protected
-     * @param  bool $event 是否需要事件响应
+     * @param bool $event 是否需要事件响应
      * @return $this
      */
     public function withEvent(bool $event)
@@ -77,7 +71,7 @@ class Event
     /**
      * 批量注册事件监听
      * @access public
-     * @param  array $events 事件定义
+     * @param array $events 事件定义
      * @return $this
      */
     public function listenEvents(array $events)
@@ -100,9 +94,9 @@ class Event
     /**
      * 注册事件监听
      * @access public
-     * @param  string $event    事件名称
-     * @param  mixed  $listener 监听操作（或者类名）
-     * @param  bool   $first    是否优先执行
+     * @param string $event    事件名称
+     * @param mixed  $listener 监听操作（或者类名）
+     * @param bool   $first    是否优先执行
      * @return $this
      */
     public function listen(string $event, $listener, bool $first = false)
@@ -127,10 +121,10 @@ class Event
     /**
      * 是否存在事件监听
      * @access public
-     * @param  string $event 事件名称
+     * @param string $event 事件名称
      * @return bool
      */
-    public function hasListen(string $event): bool
+    public function hasListener(string $event): bool
     {
         if (isset($this->bind[$event])) {
             $event = $this->bind[$event];
@@ -142,8 +136,8 @@ class Event
     /**
      * 移除事件监听
      * @access public
-     * @param  string $event 事件名称
-     * @return $this
+     * @param string $event 事件名称
+     * @return void
      */
     public function remove(string $event): void
     {
@@ -157,7 +151,7 @@ class Event
     /**
      * 指定事件别名标识 便于调用
      * @access public
-     * @param  array $events 事件别名
+     * @param array $events 事件别名
      * @return $this
      */
     public function bind(array $events)
@@ -170,8 +164,8 @@ class Event
     /**
      * 注册事件订阅者
      * @access public
-     * @param  mixed $subscriber 订阅者
-     * @param  array $events     事件列表
+     * @param mixed $subscriber 订阅者
+     * @param array $events     事件列表
      * @return $this
      */
     public function subscribe($subscriber, array $events = [])
@@ -202,8 +196,8 @@ class Event
     /**
      * 自动注册事件观察者
      * @access public
-     * @param  string|object $observer 观察者
-     * @param  array         $events   事件列表
+     * @param string|object $observer 观察者
+     * @param array         $events   事件列表
      * @return $this
      */
     public function observe($observer, array $events = [])
@@ -243,9 +237,9 @@ class Event
     /**
      * 触发事件
      * @access public
-     * @param  string|object $event  事件名称
-     * @param  mixed         $params 传入参数
-     * @param  bool          $once   只获取一个有效返回值
+     * @param string|object $event  事件名称
+     * @param mixed         $params 传入参数
+     * @param bool          $once   只获取一个有效返回值
      * @return mixed
      */
     public function trigger($event, $params = null, bool $once = false)
@@ -278,10 +272,21 @@ class Event
     }
 
     /**
+     * 触发事件(只获取一个有效返回值)
+     * @param      $event
+     * @param null $params
+     * @return mixed
+     */
+    public function until($event, $params = null)
+    {
+        return $this->trigger($event, $params, true);
+    }
+
+    /**
      * 执行事件调度
      * @access protected
-     * @param  mixed $event  事件方法
-     * @param  mixed $params 参数
+     * @param mixed $event  事件方法
+     * @param mixed $params 参数
      * @return mixed
      */
     protected function dispatch($event, $params = null)
