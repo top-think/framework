dc4eb6b045b75b5dd20d62817c9cee5c394defea
sockball <sockday@yahoo.com>
2019-12-19 14:22:16 +0800

    修正 Redis::has() 方法的返回值为bool

--- library/think/cache/driver/Redis.php
+++ library/think/cache/driver/Redis.php
@@ -70,7 +70,7 @@ class Redis extends Driver
      */
     public function has($name)
     {
-        return $this->handler->exists($this->getCacheKey($name));
+        return (bool) $this->handler->exists($this->getCacheKey($name));
     }
 
     /**
