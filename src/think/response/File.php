7a82906108b33bfa20524df47547b04b926e3831
高奕GaoYi <gaoyia@qq.com>
2020-02-26 03:29:14 +0800

    建议增加是否强制下载文件选项，有些是图片或pdf，希望浏览器能直接打开

--- src/think/response/File.php
+++ src/think/response/File.php
@@ -24,6 +24,7 @@ class File extends Response
     protected $name;
     protected $mimeType;
     protected $isContent = false;
+    protected $isForceDownload = true;
 
     public function __construct($data = '', int $code = 200)
     {
@@ -62,7 +63,7 @@ class File extends Response
         $this->header['Pragma']                    = 'public';
         $this->header['Content-Type']              = $mimeType ?: 'application/octet-stream';
         $this->header['Cache-control']             = 'max-age=' . $this->expire;
-        $this->header['Content-Disposition']       = 'attachment; filename="' . $name . '"';
+        $this->header['Content-Disposition']       = ($this->isForceDownload?'attachment; ' : '') . 'filename="' . $name . '"';
         $this->header['Content-Length']            = $size;
         $this->header['Content-Transfer-Encoding'] = 'binary';
         $this->header['Expires']                   = gmdate("D, d M Y H:i:s", time() + $this->expire) . ' GMT';
@@ -106,6 +107,17 @@ class File extends Response
     {
         $this->mimeType = $mimeType;
         return $this;
+    }
+
+    /**
+     * 设置文件强制下载
+     * @access public
+     * @param  bool $force 强制浏览器下载
+     * @return string
+     */
+    public function forceDownload(bool $force)
+    {
+        $this->isForceDownload = $force;
     }
 
     /**
