96b2eed5dc7d041c78b38da6d115a84a7f8d8ffd
auooru <auooru@163.com>
2020-12-21 14:59:54 +0800

    资源路由支持设置变量规则

--- src/think/route/Resource.php
+++ src/think/route/Resource.php
@@ -132,7 +132,7 @@ class Resource extends RuleGroup
 
             $ruleItem = $this->addRule(trim($prefix . $val[1], '/'), $this->route . '/' . $val[2], $val[0]);
 
-            foreach (['model', 'validate', 'middleware'] as $name) {
+            foreach (['model', 'validate', 'middleware', 'pattern'] as $name) {
                 if (isset($this->$name[$key])) {
                     call_user_func_array([$ruleItem, $name], (array) $this->$name[$key]);
                 }
