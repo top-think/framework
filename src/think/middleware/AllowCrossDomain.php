c2b477b358832385a82cab1bea84137622544875
ThinkPHP <liu21st@users.noreply.github.com>
2021-01-23 09:01:52 +0000

    Apply fixes from StyleCI

--- src/think/middleware/AllowCrossDomain.php
+++ src/think/middleware/AllowCrossDomain.php
@@ -44,7 +44,7 @@ class AllowCrossDomain
      * @param array   $header
      * @return Response
      */
-    public function handle($request, Closure $next,  ? array $header = [])
+    public function handle($request, Closure $next, ? array $header = [])
     {
         $header = !empty($header) ? array_merge($this->header, $header) : $this->header;
 
