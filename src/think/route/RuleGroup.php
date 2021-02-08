0764355bf2cb2d79696fd15071cccacf10d2e2e4
thinkphp <thinkphp@qq.com>
2021-02-08 16:01:54 +0800

    路由分组增加dispatcher方法 设置分组的调度

--- src/think/route/RuleGroup.php
+++ src/think/route/RuleGroup.php
@@ -171,7 +171,9 @@ class RuleGroup extends Rule
             }
         }
 
-        if ($this->miss && in_array($this->miss->getMethod(), ['*', $method])) {
+        if (!empty($option['dispatcher'])) {
+            $result = $this->parseRule($request, '', $option['dispatcher'], $url, $option);
+        } elseif ($this->miss && in_array($this->miss->getMethod(), ['*', $method])) {
             // 未匹配所有路由的路由规则处理
             $result = $this->parseRule($request, '', $this->miss->getRoute(), $url, $this->miss->getOption());
         } else {
@@ -464,6 +466,17 @@ class RuleGroup extends Rule
     public function mergeRuleRegex(bool $merge = true)
     {
         return $this->setOption('merge_rule_regex', $merge);
+    }
+
+    /**
+     * 设置分组的Dispatch调度
+     * @access public
+     * @param  string $dispatch 调度类
+     * @return $this
+     */
+    public function dispatcher(string $dispatch)
+    {
+        return $this->setOption('dispatcher', $dispatch);
     }
 
     /**
