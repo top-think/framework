bf2b55167f5a28ce338128363b8efe6bc358f450
xiaoxu <657978106@qq.com>
2021-06-24 18:34:50 +0800

    FIX 内置服务器启动默认host显示问题

--- src/think/console/command/RunServer.php
+++ src/think/console/command/RunServer.php
@@ -63,7 +63,7 @@ class RunServer extends Command
             escapeshellarg($root . DIRECTORY_SEPARATOR . 'router.php')
         );
 
-        $output->writeln(sprintf('ThinkPHP Development server is started On <http://%s:%s/>', '0.0.0.0' == $host ? '127.0.0.1' : $host, $port));
+        $output->writeln(sprintf('ThinkPHP Development server is started On <http://%s:%s/>', $host, $port));
         $output->writeln(sprintf('You can exit with <info>`CTRL-C`</info>'));
         $output->writeln(sprintf('Document root is: %s', $root));
         passthru($command);
