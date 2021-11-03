ff4913f306e2ea2b75a2a0df5f603acf1b117afc
Greatfar <panggreatfar@foxmail.com>
2021-11-03 11:27:48 +0800

    发送cookie前先检查请求头是否已发送

--- src/think/Response.php
+++ src/think/Response.php
@@ -130,16 +130,19 @@ abstract class Response
         // 处理输出数据
         $data = $this->getContent();
 
-        if (!headers_sent() && !empty($this->header)) {
-            // 发送状态码
-            http_response_code($this->code);
-            // 发送头部信息
-            foreach ($this->header as $name => $val) {
-                header($name . (!is_null($val) ? ':' . $val : ''));
+        if (!headers_sent()) {
+            if (!empty($this->header)) {
+                // 发送状态码
+                http_response_code($this->code);
+                // 发送头部信息
+                foreach ($this->header as $name => $val) {
+                    header($name . (!is_null($val) ? ':' . $val : ''));
+                }
             }
-        }
-        if ($this->cookie) {
-            $this->cookie->save();
+
+            if ($this->cookie) {
+                $this->cookie->save();
+            }
         }
 
         $this->sendData($data);
