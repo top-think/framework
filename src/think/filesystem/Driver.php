500becf1bff0d140580fa56a60cd55e0f1f2877d
cym504875043 <504875043@qq.com>
2022-06-07 15:28:59 +0800

    修复windows下上传文件失败，原因是$file->getRealPath()取不到路径。
    $stream = fopen($file->getRealPath(), 'r');修改$stream = fopen($file->getPathName(),'r');在windows和linux都可用;

--- src/think/filesystem/Driver.php
+++ src/think/filesystem/Driver.php
@@ -125,7 +125,7 @@ abstract class Driver
      */
     public function putFileAs(string $path, File $file, string $name, array $options = [])
     {
-        $stream = fopen($file->getRealPath(), 'r');
+        $stream = fopen($file->getPathName(),'r');
         $path   = trim($path . '/' . $name, '/');
 
         $result = $this->putStream($path, $stream, $options);
