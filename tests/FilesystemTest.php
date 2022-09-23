dc3b5a11d093ae78df99dfcd88e933dc30384ba4
yuanzhihai <396751927@qq.com>
2022-09-23 15:51:28 +0800

    升级 league\flysystem 2.x

--- tests/FilesystemTest.php
+++ tests/FilesystemTest.php
@@ -2,8 +2,7 @@
 
 namespace think\tests;
 
-use League\Flysystem\Adapter\NullAdapter;
-use League\Flysystem\AdapterInterface;
+use League\Flysystem\Local\LocalFilesystemAdapter;
 use Mockery as m;
 use Mockery\MockInterface;
 use org\bovigo\vfs\vfsStream;
@@ -34,15 +33,15 @@ class FilesystemTest extends TestCase
 
     protected function setUp()
     {
-        $this->app = m::mock(App::class)->makePartial();
-        Container::setInstance($this->app);
-        $this->app->shouldReceive('make')->with(App::class)->andReturn($this->app);
-        $this->config = m::mock(Config::class);
-        $this->config->shouldReceive('get')->with('filesystem.default', null)->andReturn('local');
-        $this->app->shouldReceive('get')->with('config')->andReturn($this->config);
-        $this->filesystem = new Filesystem($this->app);
+        $this->app = m::mock( App::class )->makePartial();
+        Container::setInstance( $this->app );
+        $this->app->shouldReceive( 'make' )->with( App::class )->andReturn( $this->app );
+        $this->config = m::mock( Config::class );
+        $this->config->shouldReceive( 'get' )->with( 'filesystem.default',null )->andReturn( 'local' );
+        $this->app->shouldReceive( 'get' )->with( 'config' )->andReturn( $this->config );
+        $this->filesystem = new Filesystem( $this->app );
 
-        $this->root = vfsStream::setup('rootDir');
+        $this->root = vfsStream::setup( 'rootDir' );
     }
 
     protected function tearDown(): void
@@ -52,80 +51,81 @@ class FilesystemTest extends TestCase
 
     public function testDisk()
     {
-        $this->config->shouldReceive('get')->with('filesystem.disks.local', null)->andReturn([
+        $this->config->shouldReceive( 'get' )->with( 'filesystem.disks.local',null )->andReturn( [
             'type' => 'local',
             'root' => $this->root->url(),
-        ]);
+        ] );
 
-        $this->config->shouldReceive('get')->with('filesystem.disks.foo', null)->andReturn([
+        $this->config->shouldReceive( 'get' )->with( 'filesystem.disks.foo',null )->andReturn( [
             'type' => 'local',
             'root' => $this->root->url(),
-        ]);
+        ] );
 
-        $this->assertInstanceOf(Local::class, $this->filesystem->disk());
+        $this->assertInstanceOf( Local::class,$this->filesystem->disk() );
 
-        $this->assertInstanceOf(Local::class, $this->filesystem->disk('foo'));
+        $this->assertInstanceOf( Local::class,$this->filesystem->disk( 'foo' ) );
     }
 
     public function testCache()
     {
-        $this->config->shouldReceive('get')->with('filesystem.disks.local', null)->andReturn([
+        $this->config->shouldReceive( 'get' )->with( 'filesystem.disks.local',null )->andReturn( [
             'type'  => 'local',
             'root'  => $this->root->url(),
             'cache' => true,
-        ]);
+        ] );
 
-        $this->assertInstanceOf(Local::class, $this->filesystem->disk());
+        $this->assertInstanceOf( Local::class,$this->filesystem->disk() );
 
-        $this->config->shouldReceive('get')->with('filesystem.disks.cache', null)->andReturn([
-            'type'  => NullDriver::class,
+        $this->config->shouldReceive( 'get' )->with( 'filesystem.disks.cache',null )->andReturn( [
+            'type'  => LocalDriver::class,
             'root'  => $this->root->url(),
             'cache' => [
                 'store' => 'flysystem',
             ],
-        ]);
+        ] );
 
-        $cache = m::mock(Cache::class);
+        $cache = m::mock( Cache::class );
 
-        $cacheDriver = m::mock(File::class);
+        $cacheDriver = m::mock( File::class );
 
-        $cache->shouldReceive('store')->once()->with('flysystem')->andReturn($cacheDriver);
+        $cache->shouldReceive( 'store' )->once()->with( 'flysystem' )->andReturn( $cacheDriver );
 
-        $this->app->shouldReceive('make')->with(Cache::class)->andReturn($cache);
+        $this->app->shouldReceive( 'make' )->with( Cache::class )->andReturn( $cache );
 
-        $cacheDriver->shouldReceive('get')->with('flysystem')->once()->andReturn(null);
+        $cacheDriver->shouldReceive( 'get' )->with( 'flysystem' )->once()->andReturn( null );
 
-        $cacheDriver->shouldReceive('set')->withAnyArgs();
+        $cacheDriver->shouldReceive( 'set' )->withAnyArgs();
 
-        $this->filesystem->disk('cache')->put('test.txt', 'aa');
+        $this->filesystem->disk( 'cache' )->put( 'test.txt','aa' );
     }
 
     public function testPutFile()
     {
-        $root = vfsStream::setup('rootDir', null, [
+        $root = vfsStream::setup( 'rootDir',null,[
             'foo.jpg' => 'hello',
-        ]);
+        ] );
 
-        $this->config->shouldReceive('get')->with('filesystem.disks.local', null)->andReturn([
-            'type'  => NullDriver::class,
+        $this->config->shouldReceive( 'get' )->with( 'filesystem.disks.local',null )->andReturn( [
+            'type'  => LocalDriver::class,
             'root'  => $root->url(),
             'cache' => true,
-        ]);
+        ] );
 
-        $file = m::mock(\think\File::class);
+        $file = m::mock( \think\File::class );
 
-        $file->shouldReceive('hashName')->with(null)->once()->andReturn('foo.jpg');
+        $file->shouldReceive( 'hashName' )->with( null )->once()->andReturn( 'foo.jpg' );
 
-        $file->shouldReceive('getRealPath')->once()->andReturn($root->getChild('foo.jpg')->url());
+        $file->shouldReceive( 'getRealPath' )->once()->andReturn( $root->getChild( 'foo.jpg' )->url() );
 
-        $this->filesystem->putFile('test', $file);
+        $this->filesystem->putFile( 'test',$file );
     }
 }
 
-class NullDriver extends Driver
+class LocalDriver extends Driver
 {
-    protected function createAdapter(): AdapterInterface
+    protected function createAdapter()
     {
-        return new NullAdapter();
+        $config = ['root' => ''];
+        return new LocalFilesystemAdapter( $config['root'] );
     }
 }
