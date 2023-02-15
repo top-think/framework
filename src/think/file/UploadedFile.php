a559dd66f6b3b77f0fbebdc4f0d9ff1f4278a3c5
thinkphp <thinkphp@qq.com>
2023-02-15 23:08:50 +0800

    上传类使用match替换switch

--- src/think/file/UploadedFile.php
+++ src/think/file/UploadedFile.php
@@ -81,28 +81,14 @@ class UploadedFile extends File
      */
     protected function getErrorMessage(): string
     {
-        switch ($this->error) {
-            case 1:
-            case 2:
-                $message = 'upload File size exceeds the maximum value';
-                break;
-            case 3:
-                $message = 'only the portion of file is uploaded';
-                break;
-            case 4:
-                $message = 'no file to uploaded';
-                break;
-            case 6:
-                $message = 'upload temp dir not found';
-                break;
-            case 7:
-                $message = 'file write error';
-                break;
-            default:
-                $message = 'unknown upload error';
-        }
-
-        return $message;
+        return match ($this->error) {
+            1,2     => 'upload File size exceeds the maximum value',
+            3       => 'only the portion of file is uploaded',
+            4       => 'no file to uploaded',
+            6       => 'upload temp dir not found',
+            7       => 'file write error',
+            default => 'unknown upload error',
+        };
     }
 
     /**
