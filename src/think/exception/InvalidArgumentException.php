4cf6e39f0feed5010955e9ccb6c6d072a839efe5
云无心 <448901948@qq.com>
2023-03-11 10:15:26 +0000

    修正

--- src/think/exception/InvalidArgumentException.php
+++ src/think/exception/InvalidArgumentException.php
@@ -11,12 +11,11 @@
 declare (strict_types = 1);
 namespace think\exception;
 
-use Psr\Cache\InvalidArgumentException as Psr6CacheInvalidArgumentInterface;
 use Psr\SimpleCache\InvalidArgumentException as SimpleCacheInvalidArgumentInterface;
 
 /**
  * 非法数据异常
  */
-class InvalidArgumentException extends \InvalidArgumentException implements Psr6CacheInvalidArgumentInterface, SimpleCacheInvalidArgumentInterface
+class InvalidArgumentException extends \InvalidArgumentException implements SimpleCacheInvalidArgumentInterface
 {
 }
