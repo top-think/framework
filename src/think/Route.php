780eeaa1d5976a6e538a536d96657b08df90e556
yunwuxin <448901948@qq.com>
2023-03-14 19:33:11 +0800

    options请求默认支持跨域

--- src/think/Route.php
+++ src/think/Route.php
@@ -187,7 +187,7 @@ class Route
         // 注册全局MISS路由
         $this->miss(function () {
             return Response::create('', 'html', 204)->header(['Allow' => 'GET, POST, PUT, DELETE']);
-        }, 'options');
+        }, 'options')->allowCrossDomain();
     }
 
     public function config(string $name = null)
