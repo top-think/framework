3608e06ac845b6c28abba274eb7fb8614513f091
thinkphp <thinkphp@qq.com>
2024-08-22 14:00:25 +0800

    改进异常显示

--- src/think/initializer/Error.php
+++ src/think/initializer/Error.php
@@ -8,7 +8,7 @@
 // +----------------------------------------------------------------------
 // | Author: 麦当苗儿 <zuojiazi@vip.qq.com> <http://zjzit.cn>
 // +----------------------------------------------------------------------
-declare(strict_types=1);
+declare (strict_types = 1);
 
 namespace think\initializer;
 
@@ -36,6 +36,7 @@ class Error
     {
         $this->app = $app;
         error_reporting(E_ALL);
+        ini_set('display_errors', 'Off');
         set_error_handler([$this, 'appError']);
         set_exception_handler([$this, 'appException']);
         register_shutdown_function([$this, 'appShutdown']);
