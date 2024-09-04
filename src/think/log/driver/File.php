5807cb49cbbc86e59f50ed64ee77deaa67b7babf
thinkphp <thinkphp@qq.com>
2024-09-04 15:30:21 +0800

    改进日志备份文件的命名规则

--- src/think/log/driver/File.php
+++ src/think/log/driver/File.php
@@ -198,7 +198,7 @@ class File implements LogHandlerInterface
     {
         if (is_file($destination) && floor($this->config['file_size']) <= filesize($destination)) {
             try {
-                rename($destination, dirname($destination) . DIRECTORY_SEPARATOR . time() . '-' . basename($destination));
+                rename($destination, dirname($destination) . DIRECTORY_SEPARATOR . basename($destination, '.log') . '-' . time() . '.log');
             } catch (\Exception $e) {
                 //
             }
