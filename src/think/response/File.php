573e4234ffb9bb9ad54455eef3696f70dbe32c83
xieyongfa <215005377@qq.com>
2024-10-07 09:51:42 +0800

    Update File.php
    优化文件下载在不同浏览器中的兼容性

--- src/think/response/File.php
+++ src/think/response/File.php
@@ -66,7 +66,7 @@ class File extends Response
         $this->header['Pragma']                    = 'public';
         $this->header['Content-Type']              = $mimeType ?: 'application/octet-stream';
         $this->header['Cache-control']             = 'max-age=' . $this->expire;
-        $this->header['Content-Disposition']       = ($this->force ? 'attachment; ' : '') . "filename* = UTF-8''{$name}";
+        $this->header['Content-Disposition']       = ($this->force ? 'attachment; ' : '') . 'filename="' . $name . '";' . "filename* = UTF-8''{$name}";
         $this->header['Content-Length']            = $size;
         $this->header['Content-Transfer-Encoding'] = 'binary';
         $this->header['Expires']                   = gmdate("D, d M Y H:i:s", time() + $this->expire) . ' GMT';
