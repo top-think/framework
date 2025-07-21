6d0d276dae867b894fd0fd166b426c73f48bfb47
heui <runphp@qq.com>
2025-07-21 17:23:04 +0800

    fix(config): $result未定义

--- src/think/Config.php
+++ src/think/Config.php
@@ -187,8 +187,9 @@ class Config
             if (is_null($result)) {
                 return $default;
             }
+            return $result;
         }
-        return $result;
+        return $value?:$default;
     }
 
     /**
